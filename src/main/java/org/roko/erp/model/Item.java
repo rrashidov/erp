be46a877c791678666413306c68d01fe8222aebb
Rashid Rashidov <rrashidov@gmail.com>
2022-10-20 22:34:41 +0300

    Introduce ItemRepository

--- src/main/java/org/roko/erp/model/Item.java
+++ src/main/java/org/roko/erp/model/Item.java
@@ -1,7 +1,12 @@
 package org.roko.erp.model;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 public class Item {
 
+	@Id
 	private String code;
 	
 	private String description;
---
+++ src/main/java/org/roko/erp/repositories/ItemRepository.java
@@ -0,0 +1,10 @@
+package org.roko.erp.repositories;
+
+import org.roko.erp.model.Item;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ItemRepository extends JpaRepository<Item, String> {
+    
+}
