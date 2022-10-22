e17950295420e14ff060816c9ae4a12cfee0705b
Rashid Rashidov <rrashidov@gmail.com>
2022-10-22 18:48:20 +0300

    Edit PaymentMethod model object
    - make it entity

--- src/main/java/org/roko/erp/model/PaymentMethod.java
+++ src/main/java/org/roko/erp/model/PaymentMethod.java
@@ -1,7 +1,12 @@
 package org.roko.erp.model;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 public class PaymentMethod {
 
+	@Id
 	private String code;
 	private String name;
 	private BankAccount bankAccount;
