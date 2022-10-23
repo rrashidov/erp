f5008468ea465ce74ec62eaa0c37c931289cb340
Rashid Rashidov <rrashidov@gmail.com>
2022-10-23 13:45:15 +0300

    Edit BankAccount model object
    - make it JPA entity

--- src/main/java/org/roko/erp/model/BankAccount.java
+++ src/main/java/org/roko/erp/model/BankAccount.java
@@ -1,7 +1,12 @@
 package org.roko.erp.model;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 public class BankAccount {
 
+	@Id
 	private String code;
 	private String name;
 	
