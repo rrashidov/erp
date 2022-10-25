64572dc9ab5256c475c8ec87ec5205f37b3f45c9
Rashid Rashidov <rrashidov@gmail.com>
2022-10-25 21:38:00 +0300

    Implement creating new vendors

--- src/main/java/org/roko/erp/controllers/VendorController.java
+++ src/main/java/org/roko/erp/controllers/VendorController.java
@@ -2,34 +2,65 @@ package org.roko.erp.controllers;
 
 import java.util.List;
 
+import org.roko.erp.controllers.model.VendorModel;
 import org.roko.erp.controllers.paging.PagingData;
 import org.roko.erp.controllers.paging.PagingService;
 import org.roko.erp.model.Vendor;
+import org.roko.erp.services.PaymentMethodService;
 import org.roko.erp.services.VendorService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.view.RedirectView;
 
 @Controller
 public class VendorController {
-    
+
     private VendorService vendorSvc;
     private PagingService pagingSvc;
+    private PaymentMethodService paymentMethodSvc;
 
-    public VendorController(VendorService vendorSvc, PagingService pagingSvc) {
+    public VendorController(VendorService vendorSvc, PagingService pagingSvc, PaymentMethodService paymentMethodSvc) {
         this.vendorSvc = vendorSvc;
         this.pagingSvc = pagingSvc;
-	}
+        this.paymentMethodSvc = paymentMethodSvc;
+    }
 
     @GetMapping("/vendorList")
-	public String list(@RequestParam(name="page", required = false) Long page, Model model){
+    public String list(@RequestParam(name = "page", required = false) Long page, Model model) {
         PagingData pagingData = pagingSvc.generate("vendor", page, vendorSvc.count());
         List<Vendor> vendors = vendorSvc.list();
 
         model.addAttribute("vendors", vendors);
         model.addAttribute("paging", pagingData);
-        
+
         return "vendorList.html";
+    }
+
+    @GetMapping("/vendorCard")
+    public String card(@RequestParam(name = "code", required = false) String code, Model model) {
+        VendorModel vendorModel = new VendorModel();
+
+        model.addAttribute("vendor", vendorModel);
+        model.addAttribute("paymentMethods", paymentMethodSvc.list());
+
+        return "vendorCard.html";
+    }
+
+    @PostMapping("/vendorCard")
+    public RedirectView post(@ModelAttribute VendorModel vendorModel){
+        Vendor vendor = new Vendor();
+
+        vendor.setCode(vendorModel.getCode());
+        vendor.setName(vendorModel.getName());
+        vendor.setAddress(vendorModel.getAddress());
+        vendor.setPaymentMethod(paymentMethodSvc.get(vendorModel.getPaymentMethodCode()));
+
+        vendorSvc.create(vendor);
+
+        return new RedirectView("/vendorList");
     }
 }
--- src/test/java/org/roko/erp/controllers/VendorControllerTest.java
+++ src/test/java/org/roko/erp/controllers/VendorControllerTest.java
@@ -1,6 +1,7 @@
 package org.roko.erp.controllers;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -9,13 +10,19 @@ import java.util.List;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.roko.erp.controllers.model.VendorModel;
 import org.roko.erp.controllers.paging.PagingData;
 import org.roko.erp.controllers.paging.PagingService;
+import org.roko.erp.model.PaymentMethod;
 import org.roko.erp.model.Vendor;
+import org.roko.erp.services.PaymentMethodService;
 import org.roko.erp.services.VendorService;
 import org.springframework.ui.Model;
+import org.springframework.web.servlet.view.RedirectView;
 
 public class VendorControllerTest {
 
@@ -23,7 +30,26 @@ public class VendorControllerTest {
 
     private static final long TEST_RECORD_COUNT = 234l;
 
+    private static final String TEST_CODE = "test-code";
+    private static final String TEST_NAME = "test-name";
+    private static final String TEST_ADDRESS = "test-address";
+    private static final String TEST_PAYMENT_METHOD_CODE = "test-payment-method-code";
+
     private List<Vendor> vendorList = new ArrayList<>();
+
+    private List<PaymentMethod> paymentMethodList = new ArrayList<>();
+
+    @Mock
+    private PaymentMethod paymentMethodMock;
+
+    @Mock
+    private VendorModel vendorModelMock;
+
+    @Captor
+    private ArgumentCaptor<Vendor> vendorArgumentCaptor;
+
+    @Captor
+    private ArgumentCaptor<VendorModel> vendorModelArgumentCaptor;
 
     @Mock
     private PagingData pagingDataMock;
@@ -36,6 +62,9 @@ public class VendorControllerTest {
 
     @Mock
     private PagingService pagingSvcMock;
+
+    @Mock
+    private PaymentMethodService paymentMethodSvcMock;
     
     private VendorController controller;
 
@@ -43,12 +72,20 @@ public class VendorControllerTest {
     public void setup(){
         MockitoAnnotations.openMocks(this);
 
+        when(vendorModelMock.getCode()).thenReturn(TEST_CODE);
+        when(vendorModelMock.getName()).thenReturn(TEST_NAME);
+        when(vendorModelMock.getAddress()).thenReturn(TEST_ADDRESS);
+        when(vendorModelMock.getPaymentMethodCode()).thenReturn(TEST_PAYMENT_METHOD_CODE);
+
+        when(paymentMethodSvcMock.list()).thenReturn(paymentMethodList);
+        when(paymentMethodSvcMock.get(TEST_PAYMENT_METHOD_CODE)).thenReturn(paymentMethodMock);
+
         when(vendorSvcMock.list()).thenReturn(vendorList);
         when(vendorSvcMock.count()).thenReturn(TEST_RECORD_COUNT);
 
         when(pagingSvcMock.generate("vendor", TEST_PAGE, TEST_RECORD_COUNT)).thenReturn(pagingDataMock);
 
-        controller = new VendorController(vendorSvcMock, pagingSvcMock);
+        controller = new VendorController(vendorSvcMock, pagingSvcMock, paymentMethodSvcMock);
     }
 
     @Test
@@ -59,5 +96,38 @@ public class VendorControllerTest {
 
         verify(modelMock).addAttribute("vendors", vendorList);
         verify(modelMock).addAttribute("paging", pagingDataMock);
+    }
+
+    @Test
+    public void cardReturnsProperTemplate_whenCalledForNewVendor(){
+        String template = controller.card(null, modelMock);
+
+        assertEquals("vendorCard.html", template);
+
+        verify(modelMock).addAttribute(eq("vendor"), vendorModelArgumentCaptor.capture());
+        verify(modelMock).addAttribute("paymentMethods", paymentMethodList);
+
+        VendorModel vendorModel = vendorModelArgumentCaptor.getValue();
+
+        assertEquals("", vendorModel.getCode());
+        assertEquals("", vendorModel.getName());
+        assertEquals("", vendorModel.getAddress());
+        assertEquals("", vendorModel.getPaymentMethodCode());
+    }
+
+    @Test
+    public void postingCard_createsNewVendor(){
+        RedirectView redirectView = controller.post(vendorModelMock);
+
+        assertEquals("/vendorList", redirectView.getUrl());
+
+        verify(vendorSvcMock).create(vendorArgumentCaptor.capture());
+
+        Vendor vendor = vendorArgumentCaptor.getValue();
+
+        assertEquals(TEST_CODE, vendor.getCode());
+        assertEquals(TEST_NAME, vendor.getName());
+        assertEquals(TEST_ADDRESS, vendor.getAddress());
+        assertEquals(paymentMethodMock, vendor.getPaymentMethod());
     }
 }
