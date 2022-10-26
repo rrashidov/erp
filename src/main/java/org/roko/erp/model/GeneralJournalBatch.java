3aec788629776c827a6929b15cd500b6c91c75e9
Rashid Rashidov <rrashidov@gmail.com>
2022-10-26 10:46:36 +0300

    Edit GeneralJournalBatch model object
    - make it JPA entity

--- src/main/java/org/roko/erp/model/GeneralJournalBatch.java
+++ src/main/java/org/roko/erp/model/GeneralJournalBatch.java
@@ -1,7 +1,12 @@
 package org.roko.erp.model;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 public class GeneralJournalBatch {
 
+	@Id
 	private String code;
 	private String description;
 	
