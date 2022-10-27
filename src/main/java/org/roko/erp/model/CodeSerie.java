eef0b207efad0c2e1fea78e949366e6e33cbd838
Rashid Rashidov <rrashidov@gmail.com>
2022-10-27 10:52:02 +0300

    Introduce CodeSerie domain model object

---
+++ src/main/java/org/roko/erp/model/CodeSerie.java
@@ -0,0 +1,33 @@
+package org.roko.erp.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class CodeSerie {
+    
+    @Id
+    private String code;
+
+    private String firstCode;
+    private String lastCode;
+    
+    public String getCode() {
+        return code;
+    }
+    public void setCode(String code) {
+        this.code = code;
+    }
+    public String getFirstCode() {
+        return firstCode;
+    }
+    public void setFirstCode(String firstCode) {
+        this.firstCode = firstCode;
+    }
+    public String getLastCode() {
+        return lastCode;
+    }
+    public void setLastCode(String lastCode) {
+        this.lastCode = lastCode;
+    }
+}
