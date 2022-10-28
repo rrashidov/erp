b6a23cb3d5a9f7f4df85235f938b753b49335d0f
Rashid Rashidov <rrashidov@gmail.com>
2022-10-28 14:13:37 +0300

    Edit SalesOrder
    - make it JPA entity

--- src/main/java/org/roko/erp/model/SalesOrder.java
+++ src/main/java/org/roko/erp/model/SalesOrder.java
@@ -2,11 +2,22 @@ package org.roko.erp.model;
 
 import java.util.Date;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity
 public class SalesOrder {
 
+	@Id
 	private String code;
+
+	@ManyToOne
 	private Customer customer;
+
 	private Date date;
+
+	@ManyToOne
 	private PaymentMethod paymentMethod;
 	
 	public String getCode() {
