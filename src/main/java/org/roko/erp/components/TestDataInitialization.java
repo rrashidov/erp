9fa9a6c25c24358939c6bf6933978c284ea909d7
Rashid Rashidov <rrashidov@gmail.com>
2022-10-31 13:02:00 +0200

    Extend test data
    - create test items

--- src/main/java/org/roko/erp/components/TestDataInitialization.java
+++ src/main/java/org/roko/erp/components/TestDataInitialization.java
@@ -2,12 +2,12 @@ package org.roko.erp.components;
 
 import org.roko.erp.model.BankAccount;
 import org.roko.erp.model.Customer;
+import org.roko.erp.model.Item;
 import org.roko.erp.model.PaymentMethod;
 import org.roko.erp.services.BankAccountService;
 import org.roko.erp.services.CustomerService;
+import org.roko.erp.services.ItemService;
 import org.roko.erp.services.PaymentMethodService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
@@ -19,13 +19,15 @@ public class TestDataInitialization implements ApplicationListener<ContextRefres
     private BankAccountService bankAccountSvc;
     private PaymentMethodService paymentMethodSvc;
     private CustomerService customerService;
+    private ItemService itemSvc;
 
     @Autowired
     public TestDataInitialization(BankAccountService bankAccountSvc, PaymentMethodService paymentMethodSvc,
-        CustomerService customerService) {
+        CustomerService customerService, ItemService itemSvc) {
         this.bankAccountSvc = bankAccountSvc;
         this.paymentMethodSvc = paymentMethodSvc;
         this.customerService = customerService;
+        this.itemSvc = itemSvc;
     }
 
     @Override
@@ -66,5 +68,28 @@ public class TestDataInitialization implements ApplicationListener<ContextRefres
       c1.setAddress("Test address");
       c1.setPaymentMethod(pm01);
       customerService.create(c1);
+
+      // init items
+      Item i1 = new Item();
+      i1.setCode("ITEM01");
+      i1.setDescription("Test item 01");
+      i1.setSalesPrice(1.0);
+      i1.setPurchasePrice(1.0);
+      itemSvc.create(i1);
+
+      Item i2 = new Item();
+      i2.setCode("ITEM02");
+      i2.setDescription("Test item 02");
+      i2.setSalesPrice(2.0);
+      i2.setPurchasePrice(2.0);
+      itemSvc.create(i2);
+
+      Item i3 = new Item();
+      i3.setCode("ITEM03");
+      i3.setDescription("Test item 03");
+      i3.setSalesPrice(3.0);
+      i3.setPurchasePrice(3.0);
+      itemSvc.create(i3);
+
     }
 }
