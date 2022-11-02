56c382e644c6475085c85db859f3f356c3f03dd8
Rashid Rashidov <rrashidov@gmail.com>
2022-11-02 22:26:39 +0200

    Implement creating purchase orders

--- src/main/java/org/roko/erp/controllers/PurchaseOrderController.java
+++ src/main/java/org/roko/erp/controllers/PurchaseOrderController.java
@@ -1,31 +1,46 @@
 package org.roko.erp.controllers;
 
+import java.util.Date;
 import java.util.List;
 
+import org.roko.erp.controllers.model.PurchaseOrderModel;
 import org.roko.erp.controllers.paging.PagingData;
 import org.roko.erp.controllers.paging.PagingService;
 import org.roko.erp.model.PurchaseOrder;
+import org.roko.erp.model.Vendor;
+import org.roko.erp.services.PaymentMethodService;
 import org.roko.erp.services.PurchaseOrderService;
+import org.roko.erp.services.VendorService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.view.RedirectView;
+
+import ch.qos.logback.core.net.SyslogOutputStream;
 
 @Controller
 public class PurchaseOrderController {
-    
+
     private PurchaseOrderService svc;
     private PagingService pagingSvc;
+    private VendorService vendorSvc;
+    private PaymentMethodService paymentMethodSvc;
 
     @Autowired
-    public PurchaseOrderController(PurchaseOrderService svc, PagingService pagingSvc) {
+    public PurchaseOrderController(PurchaseOrderService svc, VendorService vendorSvc,
+            PaymentMethodService paymentMethodSvc, PagingService pagingSvc) {
         this.svc = svc;
+        this.vendorSvc = vendorSvc;
+        this.paymentMethodSvc = paymentMethodSvc;
         this.pagingSvc = pagingSvc;
     }
 
     @GetMapping("/purchaseOrderList")
-    public String list(@RequestParam(name="page", required = false) Long page, Model model){
+    public String list(@RequestParam(name = "page", required = false) Long page, Model model) {
         List<PurchaseOrder> purchaseOrders = svc.list();
         PagingData pagingData = pagingSvc.generate("purchaseOrder", page, svc.count());
 
@@ -33,5 +48,48 @@ public class PurchaseOrderController {
         model.addAttribute("paging", pagingData);
 
         return "purchaseOrderList.html";
+    }
+
+    @GetMapping("/purchaseOrderWizard")
+    public String wizard(@RequestParam(name = "code", required = false) String code, Model model) {
+        PurchaseOrderModel purchaseOrderModel = new PurchaseOrderModel();
+        List<Vendor> vendors = vendorSvc.list();
+
+        model.addAttribute("purchaseOrderModel", purchaseOrderModel);
+        model.addAttribute("vendors", vendors);
+
+        return "purchaseOrderWizardFirstPage.html";
+    }
+
+    @PostMapping("/purchaseOrderWizardFirstPage")
+    public String postPurchaseOrderWizardFirstPage(@ModelAttribute PurchaseOrderModel purchaseOrderModel, Model model) {
+        Vendor vendor = vendorSvc.get(purchaseOrderModel.getVendorCode());
+
+        purchaseOrderModel.setVendorName(vendor.getName());
+        purchaseOrderModel.setDate(new Date());
+        purchaseOrderModel.setPaymentMethodCode(vendor.getPaymentMethod().getCode());
+
+        model.addAttribute("purchaseOrderModel", purchaseOrderModel);
+        model.addAttribute("paymentMethods", paymentMethodSvc.list());
+
+        return "purchaseOrderWizardSecondPage.html";
+    }
+
+    @PostMapping("/purchaseOrderWizardSecondPage")
+    public RedirectView postPurchaseOrderWizardSecondPage(@ModelAttribute PurchaseOrderModel purchaseOrderModel) {
+        PurchaseOrder purchaseOrder = fromModel(purchaseOrderModel);
+
+        svc.create(purchaseOrder);
+
+        return new RedirectView("/purchaseOrderList");
+    }
+
+    private PurchaseOrder fromModel(PurchaseOrderModel purchaseOrderModel) {
+        PurchaseOrder purchaseOrder = new PurchaseOrder();
+        purchaseOrder.setCode("PO" + System.currentTimeMillis());
+        purchaseOrder.setVendor(vendorSvc.get(purchaseOrderModel.getVendorCode()));
+        purchaseOrder.setDate(purchaseOrderModel.getDate());
+        purchaseOrder.setPaymentMethod(paymentMethodSvc.get(purchaseOrderModel.getPaymentMethodCode()));
+        return purchaseOrder;
     }
 }
--- src/test/java/org/roko/erp/controllers/PurchaseOrderControllerTest.java
+++ src/test/java/org/roko/erp/controllers/PurchaseOrderControllerTest.java
@@ -1,6 +1,7 @@
 package org.roko.erp.controllers;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -9,21 +10,56 @@ import java.util.List;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.roko.erp.controllers.model.PurchaseOrderModel;
 import org.roko.erp.controllers.paging.PagingData;
 import org.roko.erp.controllers.paging.PagingService;
+import org.roko.erp.model.PaymentMethod;
 import org.roko.erp.model.PurchaseOrder;
+import org.roko.erp.model.Vendor;
+import org.roko.erp.services.PaymentMethodService;
 import org.roko.erp.services.PurchaseOrderService;
+import org.roko.erp.services.VendorService;
 import org.springframework.ui.Model;
+import org.springframework.web.servlet.view.RedirectView;
 
 public class PurchaseOrderControllerTest {
     
+    private static final String TEST_PAYMENT_METHOD_CODE = "test-payment-method-code";
+
+    private static final String TEST_VENDOR_CODE = "test-vendor-code";
+    private static final String TEST_VENDOR_NAME = "test-vendor-name";
+
     private static final Long TEST_PAGE = 123l;
 
     private static final long TEST_COUNT = 234l;
 
     private List<PurchaseOrder> purchaseOrders = new ArrayList<>();
+
+    private List<Vendor> vendors = new ArrayList<>();
+
+    private List<PaymentMethod> paymentMethods = new ArrayList<>();
+
+    @Captor
+    private ArgumentCaptor<PurchaseOrderModel> purchaseOrderModelArgumentCaptor;
+
+    @Captor
+    private ArgumentCaptor<PurchaseOrder> purchaseOrderArgumentCaptor;
+
+    @Mock
+    private PaymentMethod paymentMethodMock;
+
+    @Mock
+    private Vendor vendorMock;
+
+    @Mock
+    private PurchaseOrderModel purchaseOrderModelMock;
+
+    @Mock
+    private VendorService vendorSvcMock;
 
     @Mock
     private PagingData pagingDataMock;
@@ -35,6 +71,9 @@ public class PurchaseOrderControllerTest {
     private PurchaseOrderService svcMock;
 
     @Mock
+    private PaymentMethodService paymentMethodSvcMock;
+
+    @Mock
     private PagingService pagingSvcMock;
 
     private PurchaseOrderController controller;
@@ -43,12 +82,26 @@ public class PurchaseOrderControllerTest {
     public void setup(){
         MockitoAnnotations.openMocks(this);
 
+        when(paymentMethodSvcMock.list()).thenReturn(paymentMethods);
+        when(paymentMethodSvcMock.get(TEST_PAYMENT_METHOD_CODE)).thenReturn(paymentMethodMock);
+
+        when(paymentMethodMock.getCode()).thenReturn(TEST_PAYMENT_METHOD_CODE);
+
+        when(vendorMock.getName()).thenReturn(TEST_VENDOR_NAME);
+        when(vendorMock.getPaymentMethod()).thenReturn(paymentMethodMock);
+
+        when(purchaseOrderModelMock.getVendorCode()).thenReturn(TEST_VENDOR_CODE);
+        when(purchaseOrderModelMock.getPaymentMethodCode()).thenReturn(TEST_PAYMENT_METHOD_CODE);
+
+        when(vendorSvcMock.list()).thenReturn(vendors);
+        when(vendorSvcMock.get(TEST_VENDOR_CODE)).thenReturn(vendorMock);
+
         when(svcMock.list()).thenReturn(purchaseOrders);
         when(svcMock.count()).thenReturn(TEST_COUNT);
 
         when(pagingSvcMock.generate("purchaseOrder", TEST_PAGE, TEST_COUNT)).thenReturn(pagingDataMock);
 
-        controller = new PurchaseOrderController(svcMock, pagingSvcMock);
+        controller = new PurchaseOrderController(svcMock, vendorSvcMock, paymentMethodSvcMock, pagingSvcMock);
     }
 
     @Test
@@ -59,5 +112,49 @@ public class PurchaseOrderControllerTest {
 
         verify(modelMock).addAttribute("purchaseOrders", purchaseOrders);
         verify(modelMock).addAttribute("paging", pagingDataMock);
+    }
+
+    @Test
+    public void wizard_returnsProperTemplate(){
+        String template = controller.wizard(null, modelMock);
+
+        assertEquals("purchaseOrderWizardFirstPage.html", template);
+
+        verify(modelMock).addAttribute(eq("purchaseOrderModel"), purchaseOrderModelArgumentCaptor.capture());
+        verify(modelMock).addAttribute("vendors", vendors);
+
+        PurchaseOrderModel purchaseOrderModel = purchaseOrderModelArgumentCaptor.getValue();
+
+        assertEquals("", purchaseOrderModel.getCode());
+        assertEquals("", purchaseOrderModel.getVendorCode());
+        assertEquals("", purchaseOrderModel.getVendorName());
+        assertEquals("", purchaseOrderModel.getPaymentMethodCode());
+    }
+
+    @Test
+    public void purchaseOrderWizardFirstPage_returnsProperTemplate(){
+        String template = controller.postPurchaseOrderWizardFirstPage(purchaseOrderModelMock, modelMock);
+
+        assertEquals("purchaseOrderWizardSecondPage.html", template);
+
+        verify(modelMock).addAttribute("purchaseOrderModel", purchaseOrderModelMock);
+        verify(modelMock).addAttribute("paymentMethods", paymentMethods);
+
+        verify(purchaseOrderModelMock).setVendorName(TEST_VENDOR_NAME);
+        verify(purchaseOrderModelMock).setPaymentMethodCode(TEST_PAYMENT_METHOD_CODE);
+    }
+
+    @Test
+    public void purchaseOrderWizardSecondPage_createsPurchaseOrder(){
+        RedirectView redirectView = controller.postPurchaseOrderWizardSecondPage(purchaseOrderModelMock);
+
+        assertEquals("/purchaseOrderList", redirectView.getUrl());
+
+        verify(svcMock).create(purchaseOrderArgumentCaptor.capture());
+
+        PurchaseOrder createdPurchaseOrder = purchaseOrderArgumentCaptor.getValue();
+
+        assertEquals(vendorMock, createdPurchaseOrder.getVendor());
+        assertEquals(paymentMethodMock, createdPurchaseOrder.getPaymentMethod());
     }
 }
