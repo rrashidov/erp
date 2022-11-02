be1af65e1afef66b463b8b96ac2468a41c266404
Rashid Rashidov <rrashidov@gmail.com>
2022-11-02 16:42:02 +0200

    Edit PurchaseOrder
    - make it Entity

--- src/main/java/org/roko/erp/model/PurchaseOrder.java
+++ src/main/java/org/roko/erp/model/PurchaseOrder.java
@@ -2,11 +2,22 @@ package org.roko.erp.model;
 
 import java.util.Date;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity
 public class PurchaseOrder {
 	
+	@Id
 	private String code;
+
+	@ManyToOne
 	private Vendor vendor;
+
 	private Date date;
+
+	@ManyToOne
 	private PaymentMethod paymentMethod;
 	
 	public String getCode() {
