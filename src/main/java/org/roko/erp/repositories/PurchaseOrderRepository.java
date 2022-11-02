79c3dc37db553e02a66403049f41c9408b471fd9
Rashid Rashidov <rrashidov@gmail.com>
2022-11-02 16:44:11 +0200

    Introduce PurchaseOrderRepository

---
+++ src/main/java/org/roko/erp/repositories/PurchaseOrderRepository.java
@@ -0,0 +1,10 @@
+package org.roko.erp.repositories;
+
+import org.roko.erp.model.PurchaseOrder;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PurchaseOrderRepository extends JpaRepository<PurchaseOrder, String> {
+    
+}
