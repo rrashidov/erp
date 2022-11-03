cafa208266de1a3862d927ed50ee2c6a4b13386c
Rashid Rashidov <rrashidov@gmail.com>
2022-11-03 16:21:17 +0200

    Edit PurchaseOrderLine
    - make it JPA entity

--- src/main/java/org/roko/erp/model/PurchaseOrderLine.java
+++ src/main/java/org/roko/erp/model/PurchaseOrderLine.java
@@ -1,42 +1,77 @@
 package org.roko.erp.model;
 
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.persistence.MapsId;
+
+import org.roko.erp.model.jpa.PurchaseOrderLineId;
+
+@Entity
 public class PurchaseOrderLine {
 
-	private String documentCode;
-	private int lineNo;
+	@EmbeddedId
+	private PurchaseOrderLineId purchaseOrderLineId;
+
+	@ManyToOne
+	@MapsId("purchaseOrder")
+	private PurchaseOrder purchaseOrder;
+
+	@ManyToOne
 	private Item item;
+
 	private double quantity;
+
+	private double price;
+
 	private double amount;
-	
-	public String getDocumentCode() {
-		return documentCode;
-	}
-	public void setDocumentCode(String documentCode) {
-		this.documentCode = documentCode;
-	}
-	public int getLineNo() {
-		return lineNo;
-	}
-	public void setLineNo(int lineNo) {
-		this.lineNo = lineNo;
-	}
+
 	public Item getItem() {
 		return item;
 	}
+
 	public void setItem(Item item) {
 		this.item = item;
 	}
+
 	public double getQuantity() {
 		return quantity;
 	}
+
 	public void setQuantity(double quantity) {
 		this.quantity = quantity;
 	}
+
 	public double getAmount() {
 		return amount;
 	}
+
 	public void setAmount(double amount) {
 		this.amount = amount;
 	}
-	
+
+	public PurchaseOrderLineId getPurchaseOrderLineId() {
+		return purchaseOrderLineId;
+	}
+
+	public void setPurchaseOrderLineId(PurchaseOrderLineId purchaseOrderLineId) {
+		this.purchaseOrderLineId = purchaseOrderLineId;
+	}
+
+	public PurchaseOrder getPurchaseOrder() {
+		return purchaseOrder;
+	}
+
+	public void setPurchaseOrder(PurchaseOrder purchaseOrder) {
+		this.purchaseOrder = purchaseOrder;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
 }
---
+++ src/main/java/org/roko/erp/model/jpa/PurchaseOrderLineId.java
@@ -0,0 +1,63 @@
+package org.roko.erp.model.jpa;
+
+import javax.persistence.Embeddable;
+import javax.persistence.ManyToOne;
+
+import org.roko.erp.model.PurchaseOrder;
+
+@Embeddable
+public class PurchaseOrderLineId {
+    
+    @ManyToOne
+    private PurchaseOrder purchaseOrder;
+
+    private int lineNo;
+
+    public PurchaseOrderLineId() {
+    }
+
+    public PurchaseOrder getPurchaseOrder() {
+        return purchaseOrder;
+    }
+
+    public void setPurchaseOrder(PurchaseOrder purchaseOrder) {
+        this.purchaseOrder = purchaseOrder;
+    }
+
+    public int getLineNo() {
+        return lineNo;
+    }
+
+    public void setLineNo(int lineNo) {
+        this.lineNo = lineNo;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((purchaseOrder == null) ? 0 : purchaseOrder.hashCode());
+        result = prime * result + lineNo;
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        PurchaseOrderLineId other = (PurchaseOrderLineId) obj;
+        if (purchaseOrder == null) {
+            if (other.purchaseOrder != null)
+                return false;
+        } else if (!purchaseOrder.equals(other.purchaseOrder))
+            return false;
+        if (lineNo != other.lineNo)
+            return false;
+        return true;
+    }
+    
+}
