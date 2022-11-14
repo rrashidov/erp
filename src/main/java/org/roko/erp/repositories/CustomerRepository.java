e63a4a6850c8a6e82a7a73d2aee101f07013899f
Rashid Rashidov <rrashidov@gmail.com>
2022-11-14 13:05:35 +0200

    [WIP] Show customer balance

--- src/main/java/org/roko/erp/repositories/CustomerRepository.java
+++ src/main/java/org/roko/erp/repositories/CustomerRepository.java
@@ -2,8 +2,13 @@ package org.roko.erp.repositories;
 
 import org.roko.erp.model.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CustomerRepository extends JpaRepository<Customer, String> {
+
+    @Query("SELECT SUM(customerLedgerEntry.amount) FROM CustomerLedgerEntry customerLedgerEntry WHERE customerLedgerEntry.customer = :customer")
+    public double balance(@Param("customer") Customer customer);
 }
