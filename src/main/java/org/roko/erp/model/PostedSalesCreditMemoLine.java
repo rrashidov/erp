bb3ea8b80b3aab2e121e17acbae5ba57e05c16b2
Rashid Rashidov <rrashidov@gmail.com>
2022-11-15 09:20:09 +0200

    Edit PostedSalesCreditMemoLine
    - make it JPA entity

--- src/main/java/org/roko/erp/model/PostedSalesCreditMemoLine.java
+++ src/main/java/org/roko/erp/model/PostedSalesCreditMemoLine.java
@@ -1,25 +1,31 @@
 package org.roko.erp.model;
 
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.persistence.MapsId;
+
+import org.roko.erp.model.jpa.PostedSalesCreditMemoLineId;
+
+@Entity
 public class PostedSalesCreditMemoLine {
 	
-	private String documentCode;
-	private int lineNo;
+	@EmbeddedId
+	private PostedSalesCreditMemoLineId postedSalesCreditMemoLineId;
+
+	@MapsId("postedSalesCreditMemo")
+	@ManyToOne
+	private PostedSalesCreditMemo postedSalesCreditMemo;
+
+	@ManyToOne
 	private Item item;
+
 	private double quantity;
+
+	private double price;
+
 	private double amount;
 	
-	public String getDocumentCode() {
-		return documentCode;
-	}
-	public void setDocumentCode(String documentCode) {
-		this.documentCode = documentCode;
-	}
-	public int getLineNo() {
-		return lineNo;
-	}
-	public void setLineNo(int lineNo) {
-		this.lineNo = lineNo;
-	}
 	public Item getItem() {
 		return item;
 	}
@@ -37,6 +43,24 @@ public class PostedSalesCreditMemoLine {
 	}
 	public void setAmount(double amount) {
 		this.amount = amount;
+	}
+	public PostedSalesCreditMemoLineId getPostedSalesCreditMemoLineId() {
+		return postedSalesCreditMemoLineId;
+	}
+	public void setPostedSalesCreditMemoLineId(PostedSalesCreditMemoLineId postedSalesCreditMemoLineId) {
+		this.postedSalesCreditMemoLineId = postedSalesCreditMemoLineId;
+	}
+	public PostedSalesCreditMemo getPostedSalesCreditMemo() {
+		return postedSalesCreditMemo;
+	}
+	public void setPostedSalesCreditMemo(PostedSalesCreditMemo postedSalesCreditMemo) {
+		this.postedSalesCreditMemo = postedSalesCreditMemo;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
 	}
 	
 }
