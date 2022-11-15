c607283861d6fe57316d2a8d7f491076cc68bb21
Rashid Rashidov <rrashidov@gmail.com>
2022-11-15 14:04:57 +0200

    Edit VendorLedgerEntry
    - make it JPA entity

--- src/main/java/org/roko/erp/model/VendorLedgerEntry.java
+++ src/main/java/org/roko/erp/model/VendorLedgerEntry.java
@@ -2,21 +2,30 @@ package org.roko.erp.model;
 
 import java.util.Date;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity
 public class VendorLedgerEntry {
 	
-	private String id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+
+	@ManyToOne
 	private Vendor vendor;
-	private Date date;
+
 	private VendorLedterEntryType type;
+
 	private double amount;
+
+	private Date date;
+
 	private String documentCode;
-	
-	public String getId() {
-		return id;
-	}
-	public void setId(String id) {
-		this.id = id;
-	}
+
 	public Vendor getVendor() {
 		return vendor;
 	}
@@ -47,7 +56,10 @@ public class VendorLedgerEntry {
 	public void setDocumentCode(String documentCode) {
 		this.documentCode = documentCode;
 	}
-	
-	
-
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
 }
