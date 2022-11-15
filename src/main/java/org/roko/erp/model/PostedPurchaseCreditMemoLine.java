048208834ed3673f6139d8bbfdb4bf28750e6252
Rashid Rashidov <rrashidov@gmail.com>
2022-11-15 21:57:42 +0200

    Edit PostedPurchaseCreditMemoLine
    - make it JPA entity

--- src/main/java/org/roko/erp/model/PostedPurchaseCreditMemoLine.java
+++ src/main/java/org/roko/erp/model/PostedPurchaseCreditMemoLine.java
@@ -1,25 +1,31 @@
 package org.roko.erp.model;
 
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.persistence.MapsId;
+
+import org.roko.erp.model.jpa.PostedPurchaseCreditMemoLineId;
+
+@Entity
 public class PostedPurchaseCreditMemoLine {
 
-	private String documentCode;
-	private int lineNo;
+	@EmbeddedId
+	private PostedPurchaseCreditMemoLineId postedPurchaseCreditMemoLineId;
+
+	@MapsId("postedPurchaseCreditMemo")
+	@ManyToOne
+	private PostedPurchaseCreditMemo postedPurchaseCreditMemo;
+
+	@ManyToOne
 	private Item item;
+
 	private double quantity;
+
+	private double price;
+
 	private double amount;
 	
-	public String getDocumentCode() {
-		return documentCode;
-	}
-	public void setDocumentCode(String documentCode) {
-		this.documentCode = documentCode;
-	}
-	public int getLineNo() {
-		return lineNo;
-	}
-	public void setLineNo(int lineNo) {
-		this.lineNo = lineNo;
-	}
 	public Item getItem() {
 		return item;
 	}
@@ -37,6 +43,24 @@ public class PostedPurchaseCreditMemoLine {
 	}
 	public void setAmount(double amount) {
 		this.amount = amount;
+	}
+	public PostedPurchaseCreditMemoLineId getPostedPurchaseCreditMemoLineId() {
+		return postedPurchaseCreditMemoLineId;
+	}
+	public void setPostedPurchaseCreditMemoLineId(PostedPurchaseCreditMemoLineId postedPurchaseCreditMemoLineId) {
+		this.postedPurchaseCreditMemoLineId = postedPurchaseCreditMemoLineId;
+	}
+	public PostedPurchaseCreditMemo getPostedPurchaseCreditMemo() {
+		return postedPurchaseCreditMemo;
+	}
+	public void setPostedPurchaseCreditMemo(PostedPurchaseCreditMemo postedPurchaseCreditMemo) {
+		this.postedPurchaseCreditMemo = postedPurchaseCreditMemo;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
 	}
 	
 }
---
+++ src/main/java/org/roko/erp/model/jpa/PostedPurchaseCreditMemoLineId.java
@@ -0,0 +1,63 @@
+package org.roko.erp.model.jpa;
+
+import java.io.Serializable;
+
+import javax.persistence.Embeddable;
+import javax.persistence.ManyToOne;
+
+import org.roko.erp.model.PostedPurchaseCreditMemo;
+
+@Embeddable
+public class PostedPurchaseCreditMemoLineId implements Serializable {
+
+    @ManyToOne
+    private PostedPurchaseCreditMemo postedPurchaseCreditMemo;
+
+    private int lineNo;
+
+    public PostedPurchaseCreditMemo getPostedPurchaseCreditMemo() {
+        return postedPurchaseCreditMemo;
+    }
+
+    public void setPostedPurchaseCreditMemo(PostedPurchaseCreditMemo postedPurchaseCreditMemo) {
+        this.postedPurchaseCreditMemo = postedPurchaseCreditMemo;
+    }
+
+    public int getLineNo() {
+        return lineNo;
+    }
+
+    public void setLineNo(int lineNo) {
+        this.lineNo = lineNo;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((postedPurchaseCreditMemo == null) ? 0 : postedPurchaseCreditMemo.hashCode());
+        result = prime * result + lineNo;
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        PostedPurchaseCreditMemoLineId other = (PostedPurchaseCreditMemoLineId) obj;
+        if (postedPurchaseCreditMemo == null) {
+            if (other.postedPurchaseCreditMemo != null)
+                return false;
+        } else if (!postedPurchaseCreditMemo.equals(other.postedPurchaseCreditMemo))
+            return false;
+        if (lineNo != other.lineNo)
+            return false;
+        return true;
+    }
+
+    
+}
