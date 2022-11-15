e84c21067b09355acce7fead89dc254eef88ca3c
Rashid Rashidov <rrashidov@gmail.com>
2022-11-15 12:58:23 +0200

    Edit PostedPurchaseOrderLine
    - make it JPA entity

--- src/main/java/org/roko/erp/model/PostedPurchaseOrderLine.java
+++ src/main/java/org/roko/erp/model/PostedPurchaseOrderLine.java
@@ -1,25 +1,31 @@
 package org.roko.erp.model;
 
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.persistence.MapsId;
+
+import org.roko.erp.model.jpa.PostedPurchaseOrderLineId;
+
+@Entity
 public class PostedPurchaseOrderLine {
 
-	private String documentCode;
-	private int lineNo;
+	@EmbeddedId
+	private PostedPurchaseOrderLineId postedPurchaseOrderLineId;
+
+	@MapsId("postedPurchaseOrder")
+	@ManyToOne
+	private PostedPurchaseOrder postedPurchaseOrder;
+
+	@ManyToOne
 	private Item item;
+
 	private double quantity;
+
+	private double price;
+
 	private double amount;
 	
-	public String getDocumentCode() {
-		return documentCode;
-	}
-	public void setDocumentCode(String documentCode) {
-		this.documentCode = documentCode;
-	}
-	public int getLineNo() {
-		return lineNo;
-	}
-	public void setLineNo(int lineNo) {
-		this.lineNo = lineNo;
-	}
 	public Item getItem() {
 		return item;
 	}
@@ -37,6 +43,24 @@ public class PostedPurchaseOrderLine {
 	}
 	public void setAmount(double amount) {
 		this.amount = amount;
+	}
+	public PostedPurchaseOrderLineId getPostedPurchaseOrderLineId() {
+		return postedPurchaseOrderLineId;
+	}
+	public void setPostedPurchaseOrderLineId(PostedPurchaseOrderLineId postedPurchaseOrderLineId) {
+		this.postedPurchaseOrderLineId = postedPurchaseOrderLineId;
+	}
+	public PostedPurchaseOrder getPostedPurchaseOrder() {
+		return postedPurchaseOrder;
+	}
+	public void setPostedPurchaseOrder(PostedPurchaseOrder postedPurchaseOrder) {
+		this.postedPurchaseOrder = postedPurchaseOrder;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
 	}
 	
 }
