53da9ff7190ea59434f70dc2fe90cfdb32fcceb1
Rashid Rashidov <rrashidov@gmail.com>
2022-11-15 21:33:41 +0200

    Implement showing posted purchase order card

--- src/main/java/org/roko/erp/controllers/PostedPurchaseOrderController.java
+++ src/main/java/org/roko/erp/controllers/PostedPurchaseOrderController.java
@@ -5,21 +5,27 @@ import java.util.List;
 import org.roko.erp.controllers.paging.PagingData;
 import org.roko.erp.controllers.paging.PagingService;
 import org.roko.erp.model.PostedPurchaseOrder;
+import org.roko.erp.model.PostedPurchaseOrderLine;
+import org.roko.erp.services.PostedPurchaseOrderLineService;
 import org.roko.erp.services.PostedPurchaseOrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class PostedPurchaseOrderController {
 
     private PostedPurchaseOrderService svc;
     private PagingService pagingSvc;
+    private PostedPurchaseOrderLineService postedPurchaseOrderLineSvc;
 
     @Autowired
-    public PostedPurchaseOrderController(PostedPurchaseOrderService svc, PagingService pagingSvc) {
+    public PostedPurchaseOrderController(PostedPurchaseOrderService svc,
+            PostedPurchaseOrderLineService postedPurchaseOrderLineSvc, PagingService pagingSvc) {
         this.svc = svc;
+        this.postedPurchaseOrderLineSvc = postedPurchaseOrderLineSvc;
         this.pagingSvc = pagingSvc;
     }
 
@@ -32,5 +38,18 @@ public class PostedPurchaseOrderController {
         model.addAttribute("paging", pagingData);
 
         return "postedPurchaseOrderList.html";
+    }
+
+    @GetMapping("/postedPurchaseOrderCard")
+    public String card(@RequestParam(name = "code") String code, Model model) {
+        PostedPurchaseOrder postedPurchaseOrder = svc.get(code);
+        List<PostedPurchaseOrderLine> postedPurchaseOrderLines = postedPurchaseOrderLineSvc.list(postedPurchaseOrder);
+        PagingData pagingData = pagingSvc.generate("postedPurchaseOrderLine", null, postedPurchaseOrderLineSvc.count(postedPurchaseOrder));
+
+        model.addAttribute("postedPurchaseOrder", postedPurchaseOrder);
+        model.addAttribute("postedPurchaseOrderLines", postedPurchaseOrderLines);
+        model.addAttribute("paging", pagingData);
+        
+        return "postedPurchaseOrderCard.html";
     }
 }
--- src/test/java/org/roko/erp/controllers/PostedPurchaseOrderControllerTest.java
+++ src/test/java/org/roko/erp/controllers/PostedPurchaseOrderControllerTest.java
@@ -14,14 +14,22 @@ import org.mockito.MockitoAnnotations;
 import org.roko.erp.controllers.paging.PagingData;
 import org.roko.erp.controllers.paging.PagingService;
 import org.roko.erp.model.PostedPurchaseOrder;
+import org.roko.erp.model.PostedPurchaseOrderLine;
+import org.roko.erp.services.PostedPurchaseOrderLineService;
 import org.roko.erp.services.PostedPurchaseOrderService;
 import org.springframework.ui.Model;
 
 public class PostedPurchaseOrderControllerTest {
-    
+
     private static final Long TEST_COUNT = 123l;
 
+    private static final String TEST_CODE = "test-code";
+
+    private static final Long TEST_LINE_COUNT = 234l;
+
     private List<PostedPurchaseOrder> postedPurchaseOrders = new ArrayList<>();
+
+    private List<PostedPurchaseOrderLine> postedPurchaseOrderLines = new ArrayList<>();
 
     @Mock
     private Model modelMock;
@@ -30,12 +38,21 @@ public class PostedPurchaseOrderControllerTest {
     private PagingData pagingDataMock;
 
     @Mock
+    private PagingData postedPurchaseOrderLinePagingDataMock;
+
+    @Mock
     private PostedPurchaseOrderService svcMock;
+
+    @Mock
+    private PostedPurchaseOrderLineService postedPurchaseOrderLineSvcMock;
 
     @Mock
     private PagingService pagingSvcMock;
 
     private PostedPurchaseOrderController controller;
+
+    @Mock
+    private PostedPurchaseOrder postedPurchaseOrderMock;
 
     @BeforeEach
     public void setup() {
@@ -43,10 +60,16 @@ public class PostedPurchaseOrderControllerTest {
 
         when(svcMock.list()).thenReturn(postedPurchaseOrders);
         when(svcMock.count()).thenReturn(TEST_COUNT);
+        when(svcMock.get(TEST_CODE)).thenReturn(postedPurchaseOrderMock);
+
+        when(postedPurchaseOrderLineSvcMock.list(postedPurchaseOrderMock)).thenReturn(postedPurchaseOrderLines);
+        when(postedPurchaseOrderLineSvcMock.count(postedPurchaseOrderMock)).thenReturn(TEST_LINE_COUNT);
 
         when(pagingSvcMock.generate("postedPurchaseOrder", null, TEST_COUNT)).thenReturn(pagingDataMock);
+        when(pagingSvcMock.generate("postedPurchaseOrderLine", null, TEST_LINE_COUNT))
+                .thenReturn(postedPurchaseOrderLinePagingDataMock);
 
-        controller = new PostedPurchaseOrderController(svcMock, pagingSvcMock);
+        controller = new PostedPurchaseOrderController(svcMock, postedPurchaseOrderLineSvcMock, pagingSvcMock);
     }
 
     @Test
@@ -57,5 +80,16 @@ public class PostedPurchaseOrderControllerTest {
 
         verify(modelMock).addAttribute("postedPurchaseOrders", postedPurchaseOrders);
         verify(modelMock).addAttribute("paging", pagingDataMock);
+    }
+
+    @Test
+    public void card_returnsProperTemplate() {
+        String template = controller.card(TEST_CODE, modelMock);
+
+        assertEquals("postedPurchaseOrderCard.html", template);
+
+        verify(modelMock).addAttribute("postedPurchaseOrder", postedPurchaseOrderMock);
+        verify(modelMock).addAttribute("postedPurchaseOrderLines", postedPurchaseOrderLines);
+        verify(modelMock).addAttribute("paging", postedPurchaseOrderLinePagingDataMock);
     }
 }
