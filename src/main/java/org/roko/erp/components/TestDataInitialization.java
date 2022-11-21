8bb4e4329c3b915febee954fc65a2fd280eaea6e
Rashid Rashidov <rrashidov@gmail.com>
2022-11-21 17:27:45 +0200

    Extend test data initialization
    - setup code series

--- src/main/java/org/roko/erp/components/TestDataInitialization.java
+++ src/main/java/org/roko/erp/components/TestDataInitialization.java
@@ -1,16 +1,20 @@
 package org.roko.erp.components;
 
 import org.roko.erp.model.BankAccount;
+import org.roko.erp.model.CodeSerie;
 import org.roko.erp.model.Customer;
 import org.roko.erp.model.GeneralJournalBatch;
 import org.roko.erp.model.Item;
 import org.roko.erp.model.PaymentMethod;
+import org.roko.erp.model.Setup;
 import org.roko.erp.model.Vendor;
 import org.roko.erp.services.BankAccountService;
+import org.roko.erp.services.CodeSerieService;
 import org.roko.erp.services.CustomerService;
 import org.roko.erp.services.GeneralJournalBatchService;
 import org.roko.erp.services.ItemService;
 import org.roko.erp.services.PaymentMethodService;
+import org.roko.erp.services.SetupService;
 import org.roko.erp.services.VendorService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationListener;
@@ -26,17 +30,22 @@ public class TestDataInitialization implements ApplicationListener<ContextRefres
   private VendorService vendorService;
   private ItemService itemSvc;
   private GeneralJournalBatchService generalJournalBatchSvc;
+  private CodeSerieService codeSerieSvc;
+  private SetupService setupSvc;
 
   @Autowired
   public TestDataInitialization(BankAccountService bankAccountSvc, PaymentMethodService paymentMethodSvc,
       GeneralJournalBatchService generalJournalBatchSvc,
-      CustomerService customerService, VendorService vendorService, ItemService itemSvc) {
+      CustomerService customerService, VendorService vendorService, ItemService itemSvc,
+      CodeSerieService codeSerieSvc, SetupService setupSvc) {
     this.bankAccountSvc = bankAccountSvc;
     this.paymentMethodSvc = paymentMethodSvc;
     this.generalJournalBatchSvc = generalJournalBatchSvc;
     this.customerService = customerService;
     this.vendorService = vendorService;
     this.itemSvc = itemSvc;
+    this.codeSerieSvc = codeSerieSvc;
+    this.setupSvc = setupSvc;
   }
 
   @Override
@@ -126,6 +135,79 @@ public class TestDataInitialization implements ApplicationListener<ContextRefres
 
     // init items
     initItems();
+
+    // init code series
+    initCodeSeries();
+  }
+
+  private void initCodeSeries() {
+    CodeSerie cs1 = new CodeSerie();
+    cs1.setCode("CS01");
+    cs1.setName("Sales Order");
+    cs1.setFirstCode("SO000000");
+    cs1.setLastCode("SO000000");
+    codeSerieSvc.create(cs1);
+
+    CodeSerie cs2 = new CodeSerie();
+    cs2.setCode("CS02");
+    cs2.setName("Sales Credit Memo");
+    cs2.setFirstCode("SCM000000");
+    cs2.setLastCode("SCM000000");
+    codeSerieSvc.create(cs2);
+
+    CodeSerie cs3 = new CodeSerie();
+    cs3.setCode("CS03");
+    cs3.setName("Posted Sales Order");
+    cs3.setFirstCode("PSO000000");
+    cs3.setLastCode("PSO000000");
+    codeSerieSvc.create(cs3);
+
+    CodeSerie cs4 = new CodeSerie();
+    cs4.setCode("CS04");
+    cs4.setName("Posted Sales Credit Memo");
+    cs4.setFirstCode("PSCM000000");
+    cs4.setLastCode("PSCM000000");
+    codeSerieSvc.create(cs4);
+
+    CodeSerie cs5 = new CodeSerie();
+    cs5.setCode("CS05");
+    cs5.setName("Purchase Order");
+    cs5.setFirstCode("PO000000");
+    cs5.setLastCode("PO000000");
+    codeSerieSvc.create(cs5);
+
+    CodeSerie cs6 = new CodeSerie();
+    cs6.setCode("CS06");
+    cs6.setName("Purchase Credit Memo");
+    cs6.setFirstCode("PCM000000");
+    cs6.setLastCode("PCM000000");
+    codeSerieSvc.create(cs6);
+
+    CodeSerie cs7 = new CodeSerie();
+    cs7.setCode("CS07");
+    cs7.setName("Posted Purchase Order");
+    cs7.setFirstCode("PPO000000");
+    cs7.setLastCode("PPO000000");
+    codeSerieSvc.create(cs7);
+
+    CodeSerie cs8 = new CodeSerie();
+    cs8.setCode("CS08");
+    cs8.setName("Posted Purchase Credit Memo");
+    cs8.setFirstCode("PPCM000000");
+    cs8.setLastCode("PPCM000000");
+    codeSerieSvc.create(cs8);
+
+    Setup setup = setupSvc.get();
+    setup.setSalesOrderCodeSerie(cs1);
+    setup.setSalesCreditMemoCodeSerie(cs2);
+    setup.setPostedSalesOrderCodeSerie(cs3);
+    setup.setPostedSalesCreditMemoCodeSerie(cs4);
+    setup.setPurchaseOrderCodeSerie(cs5);
+    setup.setPurchaseCreditMemoCodeSerie(cs6);
+    setup.setPostedPurchaseOrderCodeSerie(cs7);
+    setup.setPostedPurchaseCreditMemoCodeSerie(cs8);
+
+    setupSvc.update(setup);
   }
 
   private void initBulkVendors(PaymentMethod pm01) {
@@ -135,7 +217,7 @@ public class TestDataInitialization implements ApplicationListener<ContextRefres
       v1.setName("Vendor " + i);
       v1.setAddress("Address " + i);
       v1.setPaymentMethod(pm01);
-      vendorService.create(v1);  
+      vendorService.create(v1);
     }
   }
 
@@ -146,7 +228,7 @@ public class TestDataInitialization implements ApplicationListener<ContextRefres
       c1.setName("Customer " + i);
       c1.setAddress("Test address");
       c1.setPaymentMethod(pm01);
-      customerService.create(c1);  
+      customerService.create(c1);
     }
   }
 
@@ -155,7 +237,7 @@ public class TestDataInitialization implements ApplicationListener<ContextRefres
       GeneralJournalBatch genJournalBatch = new GeneralJournalBatch();
       genJournalBatch.setCode("GEN" + i);
       genJournalBatch.setName("General Journal Batch " + i);
-      
+
       generalJournalBatchSvc.create(genJournalBatch);
     }
   }
