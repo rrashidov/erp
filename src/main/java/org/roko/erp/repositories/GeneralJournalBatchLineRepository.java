300ecae971d613e0cc2c8f97f9b3e9b12ca01657
Rashid Rashidov <rrashidov@gmail.com>
2022-11-23 22:31:42 +0200

    Introduce GeneralJournalBatchLineService

--- src/main/java/org/roko/erp/repositories/GeneralJournalBatchLineRepository.java
+++ src/main/java/org/roko/erp/repositories/GeneralJournalBatchLineRepository.java
@@ -1,11 +1,20 @@
 package org.roko.erp.repositories;
 
+import org.roko.erp.model.GeneralJournalBatch;
 import org.roko.erp.model.GeneralJournalBatchLine;
 import org.roko.erp.model.jpa.GeneralJournalBatchLineId;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface GeneralJournalBatchLineRepository extends JpaRepository<GeneralJournalBatchLine, GeneralJournalBatchLineId> {
-    
+public interface GeneralJournalBatchLineRepository
+        extends JpaRepository<GeneralJournalBatchLine, GeneralJournalBatchLineId> {
+
+    @Query("SELECT generalJournalBatchLine FROM GeneralJournalBatchLine generalJournalBatchLine WHERE generalJournalBatchLine.generalJournalBatch = :generalJournalbatch")
+    public Page<GeneralJournalBatchLine> findFor(@Param("generalJournalbatch") GeneralJournalBatch generalJournalBatch,
+            Pageable pageable);
 }
---
+++ src/test/java/org/roko/erp/services/GeneralJournalBatchLineServiceTest.java
@@ -0,0 +1,110 @@
+package org.roko.erp.services;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.Optional;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.roko.erp.controllers.paging.PagingServiceImpl;
+import org.roko.erp.model.GeneralJournalBatch;
+import org.roko.erp.model.GeneralJournalBatchLine;
+import org.roko.erp.model.jpa.GeneralJournalBatchLineId;
+import org.roko.erp.repositories.GeneralJournalBatchLineRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+public class GeneralJournalBatchLineServiceTest {
+    
+    private static final int TEST_PAGE = 123;
+
+    @Captor
+    private ArgumentCaptor<Pageable> pageableArgumentCaptor;
+
+    @Mock
+    private GeneralJournalBatch generalJournalBatchMock;
+
+    @Mock
+    private GeneralJournalBatchLineId idMock;
+
+    @Mock
+    private GeneralJournalBatchLineId nonExistingId;
+
+    @Mock
+    private GeneralJournalBatchLine generalJournalBatchLineMock;
+
+    @Mock
+    private GeneralJournalBatchLineRepository repoMock;
+
+    @Mock
+    private Page<GeneralJournalBatchLine> pageMock;
+
+    private GeneralJournalBatchLineService svc;
+
+    @BeforeEach
+    public void setup() {
+        MockitoAnnotations.openMocks(this);
+
+        when(repoMock.findById(idMock)).thenReturn(Optional.of(generalJournalBatchLineMock));
+        when(repoMock.findFor(eq(generalJournalBatchMock), any(Pageable.class))).thenReturn(pageMock);
+
+        svc = new GeneralJournalBatchLineServiceImpl(repoMock);
+    }
+
+    @Test
+    public void create_delegatesToRepo() {
+        svc.create(generalJournalBatchLineMock);
+
+        verify(repoMock).save(generalJournalBatchLineMock);
+    }
+
+    @Test
+    public void update_delegatesToRepo() {
+        svc.update(idMock, generalJournalBatchLineMock);
+
+        verify(repoMock).findById(idMock);
+        verify(repoMock).save(generalJournalBatchLineMock);
+    }
+
+    @Test
+    public void get_delegatesToRepo() {
+        svc.get(idMock);
+
+        verify(repoMock).findById(idMock);
+    }
+
+    @Test
+    public void getReturnsNull_whenCalledWithNonExistingId() {
+        GeneralJournalBatchLine generalJournalBatchLine = svc.get(nonExistingId);
+
+        assertNull(generalJournalBatchLine);
+    }
+
+    @Test
+    public void list_delegatesToRepo() {
+        svc.list(generalJournalBatchMock, TEST_PAGE);
+
+        verify(repoMock).findFor(eq(generalJournalBatchMock), pageableArgumentCaptor.capture());
+
+        Pageable pageable = pageableArgumentCaptor.getValue();
+
+        assertEquals(TEST_PAGE - 1, pageable.getPageNumber());
+        assertEquals(PagingServiceImpl.RECORDS_PER_PAGE, pageable.getPageSize());
+    }
+
+    @Test
+    public void delete_delegatesToRepo() {
+        svc.delete(idMock);
+
+        verify(repoMock).delete(generalJournalBatchLineMock);
+    }
+}
