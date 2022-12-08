34993ce601f4b10be8ee0f7fb64b590801132b8d
Rashid Rashidov <rrashidov@gmail.com>
2022-12-08 16:49:38 +0200

    [backend] Code cleanup in SalesOrderController
    - remove unused import

--- erp.backend/src/main/java/org/roko/erp/backend/controllers/SalesOrderController.java
+++ erp.backend/src/main/java/org/roko/erp/backend/controllers/SalesOrderController.java
@@ -20,8 +20,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.fasterxml.jackson.annotation.JsonTypeInfo.Id;
-
 @RestController
 @RequestMapping("/api/v1/salesorders")
 public class SalesOrderController {
