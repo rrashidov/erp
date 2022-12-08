8b3a182adb24eb8843b76084c2121a88c99c5cc8
Rashid Rashidov <rrashidov@gmail.com>
2022-12-08 16:48:00 +0200

    [backend] Extend SalesOrderController
    - add endpoints to manage sales order lines

--- erp.backend/src/main/java/org/roko/erp/backend/controllers/SalesOrderController.java
+++ erp.backend/src/main/java/org/roko/erp/backend/controllers/SalesOrderController.java
@@ -4,8 +4,12 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import org.roko.erp.backend.model.SalesOrder;
+import org.roko.erp.backend.model.SalesOrderLine;
+import org.roko.erp.backend.model.jpa.SalesOrderLineId;
+import org.roko.erp.backend.services.SalesOrderLineService;
 import org.roko.erp.backend.services.SalesOrderService;
 import org.roko.erp.model.dto.SalesOrderDTO;
+import org.roko.erp.model.dto.SalesOrderLineDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,15 +20,19 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.fasterxml.jackson.annotation.JsonTypeInfo.Id;
+
 @RestController
 @RequestMapping("/api/v1/salesorders")
 public class SalesOrderController {
 
     private SalesOrderService svc;
+    private SalesOrderLineService salesOrderLineSvc;
 
     @Autowired
-    public SalesOrderController(SalesOrderService svc) {
+    public SalesOrderController(SalesOrderService svc, SalesOrderLineService salesOrderLineSvc) {
         this.svc = svc;
+        this.salesOrderLineSvc = salesOrderLineSvc;
     }
 
     @GetMapping("/page/{page}")
@@ -37,6 +45,63 @@ public class SalesOrderController {
     @GetMapping("/{code}")
     public SalesOrderDTO get(@PathVariable("code") String code) {
         return svc.toDTO(svc.get(code));
+    }
+
+    @GetMapping("/{code}/lines/page/{page}")
+    public List<SalesOrderLineDTO> listLines(@PathVariable("code") String code, @PathVariable("page") int page) {
+        SalesOrder salesOrder = svc.get(code);
+        return salesOrderLineSvc.list(salesOrder, page).stream()
+                .map(x -> salesOrderLineSvc.toDTO(x))
+                .collect(Collectors.toList());
+    }
+
+    @GetMapping("/{code}/lines/{lineNo}")
+    public SalesOrderLineDTO getLine(@PathVariable("code") String code, @PathVariable("lineNo") int lineNo) {
+        SalesOrder salesOrder = svc.get(code);
+
+        SalesOrderLineId salesOrderLineId = new SalesOrderLineId();
+        salesOrderLineId.setSalesOrder(salesOrder);
+        salesOrderLineId.setLineNo(lineNo);
+
+        return salesOrderLineSvc.toDTO(salesOrderLineSvc.get(salesOrderLineId));
+    }
+
+    @PostMapping("/{code}/lines")
+    public int postLine(@PathVariable("code") String code, @RequestBody SalesOrderLineDTO salesOrderLineDTO) {
+        SalesOrderLine salesOrderLine = salesOrderLineSvc.fromDTO(salesOrderLineDTO);
+
+        salesOrderLineSvc.create(salesOrderLine);
+
+        return salesOrderLine.getSalesOrderLineId().getLineNo();
+    }
+
+    @PutMapping("/{code}/lines/{lineNo}")
+    public int putLine(@PathVariable("code") String code, @PathVariable("lineNo") int lineNo,
+            @RequestBody SalesOrderLineDTO salesOrderLineDTO) {
+        SalesOrder salesOrder = svc.get(code);
+
+        SalesOrderLineId salesOrderLineId = new SalesOrderLineId();
+        salesOrderLineId.setSalesOrder(salesOrder);
+        salesOrderLineId.setLineNo(lineNo);
+
+        SalesOrderLine salesOrderLine = salesOrderLineSvc.fromDTO(salesOrderLineDTO);
+
+        salesOrderLineSvc.update(salesOrderLineId, salesOrderLine);
+
+        return lineNo;
+    }
+
+    @DeleteMapping("/{code}/lines/{lineNo}")
+    public int deleteLine(@PathVariable("code") String code, @PathVariable("lineNo") int lineNo) {
+        SalesOrder salesOrder = svc.get(code);
+
+        SalesOrderLineId salesOrderLineId = new SalesOrderLineId();
+        salesOrderLineId.setSalesOrder(salesOrder);
+        salesOrderLineId.setLineNo(lineNo);
+
+        salesOrderLineSvc.delete(salesOrderLineId);
+
+        return lineNo;
     }
 
     @PostMapping
--- erp.backend/src/test/java/org/roko/erp/backend/controllers/SalesOrderControllerTest.java
+++ erp.backend/src/test/java/org/roko/erp/backend/controllers/SalesOrderControllerTest.java
@@ -1,5 +1,7 @@
 package org.roko.erp.backend.controllers;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -7,17 +9,28 @@ import java.util.Arrays;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.roko.erp.backend.model.SalesOrder;
+import org.roko.erp.backend.model.SalesOrderLine;
+import org.roko.erp.backend.model.jpa.SalesOrderLineId;
+import org.roko.erp.backend.services.SalesOrderLineService;
 import org.roko.erp.backend.services.SalesOrderService;
 import org.roko.erp.model.dto.SalesOrderDTO;
+import org.roko.erp.model.dto.SalesOrderLineDTO;
 
 public class SalesOrderControllerTest {
     
     private static final String TEST_CODE = "test-code";
 
     private static final int TEST_PAGE = 123;
+
+    private static final int TEST_LINE_NO = 234;
+
+    @Captor
+    private ArgumentCaptor<SalesOrderLineId> salesOrderLineIdArgumentCaptor;
 
     @Mock
     private SalesOrder salesOrderMock;
@@ -28,17 +41,35 @@ public class SalesOrderControllerTest {
     @Mock
     private SalesOrderService svcMock;
 
+    @Mock
+    private SalesOrderLineService salesOrderLineSvcMock;
+
+    @Mock
+    private SalesOrderLine salesOrderLineMock;
+
     private SalesOrderController controller;
+
+    private SalesOrderLineDTO salesOrderLineDtoMock;
 
     @BeforeEach
     public void setup(){
         MockitoAnnotations.openMocks(this);
 
+        SalesOrderLineId salesOrderLineId = new SalesOrderLineId();
+        salesOrderLineId.setSalesOrder(salesOrderMock);
+        salesOrderLineId.setLineNo(TEST_LINE_NO);
+
+        when(salesOrderLineMock.getSalesOrderLineId()).thenReturn(salesOrderLineId);
+
+        when(salesOrderLineSvcMock.list(salesOrderMock, TEST_PAGE)).thenReturn(Arrays.asList(salesOrderLineMock));
+        when(salesOrderLineSvcMock.get(salesOrderLineId)).thenReturn(salesOrderLineMock);
+        when(salesOrderLineSvcMock.fromDTO(salesOrderLineDtoMock)).thenReturn(salesOrderLineMock);
+
         when(svcMock.list(TEST_PAGE)).thenReturn(Arrays.asList(salesOrderMock));
         when(svcMock.get(TEST_CODE)).thenReturn(salesOrderMock);
         when(svcMock.fromDTO(salesOrderDtoMock)).thenReturn(salesOrderMock);
 
-        controller = new SalesOrderController(svcMock);
+        controller = new SalesOrderController(svcMock, salesOrderLineSvcMock);
     }
 
     @Test
@@ -55,6 +86,59 @@ public class SalesOrderControllerTest {
 
         verify(svcMock).get(TEST_CODE);
         verify(svcMock).toDTO(salesOrderMock);
+    }
+
+    @Test
+    public void listLines_delegatesToService() {
+        controller.listLines(TEST_CODE, TEST_PAGE);
+
+        verify(salesOrderLineSvcMock).list(salesOrderMock, TEST_PAGE);
+        verify(salesOrderLineSvcMock).toDTO(salesOrderLineMock);
+    }
+
+    @Test
+    public void getLine_delegatesToService() {
+        controller.getLine(TEST_CODE, TEST_LINE_NO);
+
+        verify(salesOrderLineSvcMock).get(salesOrderLineIdArgumentCaptor.capture());
+
+        SalesOrderLineId salesOrderLineId = salesOrderLineIdArgumentCaptor.getValue();
+
+        assertEquals(salesOrderMock, salesOrderLineId.getSalesOrder());
+        assertEquals(TEST_LINE_NO, salesOrderLineId.getLineNo());
+
+        verify(salesOrderLineSvcMock).toDTO(salesOrderLineMock);
+    }
+
+    @Test
+    public void postLine_delegatesToService() {
+        controller.postLine(TEST_CODE, salesOrderLineDtoMock);
+
+        verify(salesOrderLineSvcMock).create(salesOrderLineMock);
+    }
+
+    @Test
+    public void putLine_delegatesToService() {
+        controller.putLine(TEST_CODE, TEST_LINE_NO, salesOrderLineDtoMock);
+
+        verify(salesOrderLineSvcMock).update(salesOrderLineIdArgumentCaptor.capture(), eq(salesOrderLineMock));
+
+        SalesOrderLineId salesOrderLineId = salesOrderLineIdArgumentCaptor.getValue();
+
+        assertEquals(salesOrderMock, salesOrderLineId.getSalesOrder());
+        assertEquals(TEST_LINE_NO, salesOrderLineId.getLineNo());
+    }
+
+    @Test
+    public void deleteLine_delegatesToService() {
+        controller.deleteLine(TEST_CODE, TEST_LINE_NO);
+
+        verify(salesOrderLineSvcMock).delete(salesOrderLineIdArgumentCaptor.capture());
+
+        SalesOrderLineId salesOrderLineId = salesOrderLineIdArgumentCaptor.getValue();
+
+        assertEquals(salesOrderMock, salesOrderLineId.getSalesOrder());
+        assertEquals(TEST_LINE_NO, salesOrderLineId.getLineNo());
     }
 
     @Test
