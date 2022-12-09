dd4b4d97167bb127e17b3dae2b8f94aa34189d81
Rashid Rashidov <rrashidov@gmail.com>
2022-12-09 16:14:34 +0200

    [backend] Introduce PurchaseOrderController

---
+++ erp.backend/src/main/java/org/roko/erp/backend/controllers/PurchaseOrderController.java
@@ -0,0 +1,68 @@
+package org.roko.erp.backend.controllers;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.roko.erp.backend.model.PurchaseOrder;
+import org.roko.erp.backend.services.PurchaseOrderService;
+import org.roko.erp.model.dto.PurchaseDocumentDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/purchaseorders")
+public class PurchaseOrderController {
+
+    private PurchaseOrderService svc;
+
+    @Autowired
+    public PurchaseOrderController(PurchaseOrderService svc) {
+        this.svc = svc;
+    }
+
+    @GetMapping
+    public List<PurchaseDocumentDTO> list() {
+        return svc.list().stream()
+            .map(x -> svc.toDTO(x))
+            .collect(Collectors.toList());
+    }
+
+    @GetMapping("/page/{page}")
+    public List<PurchaseDocumentDTO> list(@PathVariable("page") int page) {
+        return svc.list(page).stream()
+            .map(x -> svc.toDTO(x))
+            .collect(Collectors.toList());
+    }
+
+    @GetMapping("/{code}")
+    public PurchaseDocumentDTO get(@PathVariable("code") String code){
+        return svc.toDTO(svc.get(code));
+    }
+
+    @PostMapping
+    public String post(@RequestBody PurchaseDocumentDTO dto) {
+        PurchaseOrder purchaseOrder = svc.fromDTO(dto);
+        svc.create(purchaseOrder);
+        return purchaseOrder.getCode();
+    }
+
+    @PutMapping("/{code}")
+    public String put(@PathVariable("code") String code, @RequestBody PurchaseDocumentDTO dto) {
+        PurchaseOrder purchaseOrder = svc.fromDTO(dto);
+        svc.update(code, purchaseOrder);
+        return code;
+    }
+
+    @DeleteMapping("/{code}")
+    public String delete(@PathVariable("code") String code){
+        svc.delete(code);
+        return code;
+    }
+}
---
+++ erp.backend/src/test/java/org/roko/erp/backend/controllers/PurchaseOrderControllerTest.java
@@ -0,0 +1,90 @@
+package org.roko.erp.backend.controllers;
+
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.Arrays;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.roko.erp.backend.model.PurchaseOrder;
+import org.roko.erp.backend.services.PurchaseOrderService;
+import org.roko.erp.model.dto.PurchaseDocumentDTO;
+
+public class PurchaseOrderControllerTest {
+    
+    private static final String TEST_CODE = "test-code";
+
+    private static final int TEST_PAGE = 123;
+
+    @Mock
+    private PurchaseOrderService svcMock;
+
+    @Mock
+    private PurchaseOrder purchaseOrderMock;
+
+    @Mock
+    private PurchaseDocumentDTO dtoMock;
+
+    private PurchaseOrderController controller;
+
+    @BeforeEach
+    public void setup() {
+        MockitoAnnotations.openMocks(this);
+
+        when(svcMock.list()).thenReturn(Arrays.asList(purchaseOrderMock));
+        when(svcMock.list(TEST_PAGE)).thenReturn(Arrays.asList(purchaseOrderMock));
+        when(svcMock.get(TEST_CODE)).thenReturn(purchaseOrderMock);
+        when(svcMock.toDTO(purchaseOrderMock)).thenReturn(dtoMock);
+        when(svcMock.fromDTO(dtoMock)).thenReturn(purchaseOrderMock);
+
+        controller = new PurchaseOrderController(svcMock);
+    }
+
+    @Test
+    public void list_delegatesToService() {
+        controller.list();
+
+        verify(svcMock).list();
+        verify(svcMock).toDTO(purchaseOrderMock);
+    }
+
+    @Test
+    public void listWithPage_delegatesToService() {
+        controller.list(TEST_PAGE);
+
+        verify(svcMock).list(TEST_PAGE);
+        verify(svcMock).toDTO(purchaseOrderMock);
+    }
+
+    @Test
+    public void get_delegatesToService() {
+        controller.get(TEST_CODE);
+
+        verify(svcMock).get(TEST_CODE);
+        verify(svcMock).toDTO(purchaseOrderMock);
+    }
+
+    @Test
+    public void post_delegatesToService() {
+        controller.post(dtoMock);
+
+        verify(svcMock).create(purchaseOrderMock);
+    }
+
+    @Test
+    public void put_delegatesToService() {
+        controller.put(TEST_CODE, dtoMock);
+
+        verify(svcMock).update(TEST_CODE, purchaseOrderMock);
+    }
+
+    @Test
+    public void delete_delegatesToService() {
+        controller.delete(TEST_CODE);
+
+        verify(svcMock).delete(TEST_CODE);
+    }
+}
