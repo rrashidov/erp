920f7fd9c0f882401612e3868a81523f368687ee
Rashid Rashidov <rrashidov@gmail.com>
2022-12-09 13:08:41 +0200

    [backend] Extend PostedSalesCreditMemoController
    - add method for listing posted sales credit memo lines

--- erp.backend/src/main/java/org/roko/erp/backend/controllers/PostedSalesCreditMemoController.java
+++ erp.backend/src/main/java/org/roko/erp/backend/controllers/PostedSalesCreditMemoController.java
@@ -3,8 +3,11 @@ package org.roko.erp.backend.controllers;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.roko.erp.backend.model.PostedSalesCreditMemo;
+import org.roko.erp.backend.services.PostedSalesCreditMemoLineService;
 import org.roko.erp.backend.services.PostedSalesCreditMemoService;
 import org.roko.erp.model.dto.PostedSalesDocumentDTO;
+import org.roko.erp.model.dto.PostedSalesDocumentLineDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,13 +17,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v1/postedsalescreditmemos")
 public class PostedSalesCreditMemoController {
-    
+
     private PostedSalesCreditMemoService svc;
+    private PostedSalesCreditMemoLineService postedSalesCreditMemoLineSvc;
 
     @Autowired
-    public PostedSalesCreditMemoController(PostedSalesCreditMemoService svc) {
+    public PostedSalesCreditMemoController(PostedSalesCreditMemoService svc,
+            PostedSalesCreditMemoLineService postedSalesCreditMemoLineSvc) {
         this.svc = svc;
-	}
+        this.postedSalesCreditMemoLineSvc = postedSalesCreditMemoLineSvc;
+    }
 
     @GetMapping("/page/{page}")
     public List<PostedSalesDocumentDTO> list(@PathVariable("page") int page) {
@@ -32,5 +38,14 @@ public class PostedSalesCreditMemoController {
     @GetMapping("/{code}")
     public PostedSalesDocumentDTO get(@PathVariable("code") String code) {
         return svc.toDTO(svc.get(code));
+    }
+
+    @GetMapping("/{code}/lines/page/{page}")
+    public List<PostedSalesDocumentLineDTO> listLines(@PathVariable("code") String code,
+            @PathVariable("page") int page) {
+        PostedSalesCreditMemo postedSalesCreditMemo = svc.get(code);
+        return postedSalesCreditMemoLineSvc.list(postedSalesCreditMemo, page).stream()
+                .map(x -> postedSalesCreditMemoLineSvc.toDTO(x))
+                .collect(Collectors.toList());
     }
 }
--- erp.backend/src/test/java/org/roko/erp/backend/controllers/PostedSalesCreditMemoControllerTest.java
+++ erp.backend/src/test/java/org/roko/erp/backend/controllers/PostedSalesCreditMemoControllerTest.java
@@ -10,6 +10,8 @@ import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.roko.erp.backend.model.PostedSalesCreditMemo;
+import org.roko.erp.backend.model.PostedSalesCreditMemoLine;
+import org.roko.erp.backend.services.PostedSalesCreditMemoLineService;
 import org.roko.erp.backend.services.PostedSalesCreditMemoService;
 
 public class PostedSalesCreditMemoControllerTest {
@@ -24,16 +26,24 @@ public class PostedSalesCreditMemoControllerTest {
     @Mock
     private PostedSalesCreditMemo postedSalesCreditMemoMock;
 
+    @Mock
+    private PostedSalesCreditMemoLineService postedSalesCreditMemoLineSvcMock;
+
+    @Mock
+    private PostedSalesCreditMemoLine postedSalesCreditMemoLineMock;
+
     private PostedSalesCreditMemoController controller;
 
     @BeforeEach
     public void setup() {
         MockitoAnnotations.openMocks(this);
 
+        when(postedSalesCreditMemoLineSvcMock.list(postedSalesCreditMemoMock, TEST_PAGE)).thenReturn(Arrays.asList(postedSalesCreditMemoLineMock));
+
         when(svcMock.list(TEST_PAGE)).thenReturn(Arrays.asList(postedSalesCreditMemoMock));
         when(svcMock.get(TEST_CODE)).thenReturn(postedSalesCreditMemoMock);
         
-        controller = new PostedSalesCreditMemoController(svcMock);
+        controller = new PostedSalesCreditMemoController(svcMock, postedSalesCreditMemoLineSvcMock);
     }
 
     @Test
@@ -50,5 +60,13 @@ public class PostedSalesCreditMemoControllerTest {
 
         verify(svcMock).get(TEST_CODE);
         verify(svcMock).toDTO(postedSalesCreditMemoMock);
+    }
+
+    @Test
+    public void listLines_delegatesToService() {
+        controller.listLines(TEST_CODE, TEST_PAGE);
+
+        verify(postedSalesCreditMemoLineSvcMock).list(postedSalesCreditMemoMock, TEST_PAGE);
+        verify(postedSalesCreditMemoLineSvcMock).toDTO(postedSalesCreditMemoLineMock);
     }
 }
