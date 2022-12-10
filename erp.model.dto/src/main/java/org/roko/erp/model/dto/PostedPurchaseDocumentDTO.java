d8f8f2ca65362db8ff25c70774a0803c044ca4d1
Rashid Rashidov <rrashidov@gmail.com>
2022-12-10 20:15:05 +0200

    Introduce posted purchase document dtos

---
+++ erp.model.dto/src/main/java/org/roko/erp/model/dto/PostedPurchaseDocumentDTO.java
@@ -0,0 +1,58 @@
+package org.roko.erp.model.dto;
+
+import java.util.Date;
+
+public class PostedPurchaseDocumentDTO {
+    
+    private String code;
+    private String vendorCode;
+    private String vendorName;
+    private Date date;
+    private String paymentMethodCode;
+    private String paymentMethodName;
+    private double amount;
+
+    public String getCode() {
+        return code;
+    }
+    public void setCode(String code) {
+        this.code = code;
+    }
+    public String getVendorCode() {
+        return vendorCode;
+    }
+    public void setVendorCode(String vendorCode) {
+        this.vendorCode = vendorCode;
+    }
+    public String getVendorName() {
+        return vendorName;
+    }
+    public void setVendorName(String vendorName) {
+        this.vendorName = vendorName;
+    }
+    public Date getDate() {
+        return date;
+    }
+    public void setDate(Date date) {
+        this.date = date;
+    }
+    public String getPaymentMethodCode() {
+        return paymentMethodCode;
+    }
+    public void setPaymentMethodCode(String paymentMethodCode) {
+        this.paymentMethodCode = paymentMethodCode;
+    }
+    public String getPaymentMethodName() {
+        return paymentMethodName;
+    }
+    public void setPaymentMethodName(String paymentMethodName) {
+        this.paymentMethodName = paymentMethodName;
+    }
+    public double getAmount() {
+        return amount;
+    }
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+}
