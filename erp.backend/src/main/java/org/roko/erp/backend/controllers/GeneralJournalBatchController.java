3c9b02fad280529ae53872d1a25c5f0f3b7de724
Rashid Rashidov <rrashidov@gmail.com>
2022-12-11 21:29:41 +0200

    [backend] Extend GeneralJournalBatchController
    - introduce endpoints to manage general journal batch lines

--- erp.backend/src/main/java/org/roko/erp/backend/controllers/GeneralJournalBatchController.java
+++ erp.backend/src/main/java/org/roko/erp/backend/controllers/GeneralJournalBatchController.java
@@ -4,8 +4,12 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import org.roko.erp.backend.model.GeneralJournalBatch;
+import org.roko.erp.backend.model.GeneralJournalBatchLine;
+import org.roko.erp.backend.model.jpa.GeneralJournalBatchLineId;
+import org.roko.erp.backend.services.GeneralJournalBatchLineService;
 import org.roko.erp.backend.services.GeneralJournalBatchService;
 import org.roko.erp.model.dto.GeneralJournalBatchDTO;
+import org.roko.erp.model.dto.GeneralJournalBatchLineDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,24 +23,81 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v1/generaljournalbatches")
 public class GeneralJournalBatchController {
-    
+
     private GeneralJournalBatchService svc;
+    private GeneralJournalBatchLineService generalJournalBatchLineSvc;
 
     @Autowired
-    public GeneralJournalBatchController(GeneralJournalBatchService svc) {
+    public GeneralJournalBatchController(GeneralJournalBatchService svc,
+            GeneralJournalBatchLineService generalJournalBatchLineSvc) {
         this.svc = svc;
+        this.generalJournalBatchLineSvc = generalJournalBatchLineSvc;
     }
 
     @GetMapping("/pages/{page}")
     public List<GeneralJournalBatchDTO> list(@PathVariable("page") int page) {
         return svc.list(page).stream()
-            .map(x -> svc.toDTO(x))
-            .collect(Collectors.toList());
+                .map(x -> svc.toDTO(x))
+                .collect(Collectors.toList());
     }
 
     @GetMapping("/{code}")
     public GeneralJournalBatchDTO get(@PathVariable("code") String code) {
         return svc.toDTO(svc.get(code));
+    }
+
+    @GetMapping("/{code}/lines/page/{page}")
+    public List<GeneralJournalBatchLineDTO> listLines(@PathVariable("code") String code,
+            @PathVariable("page") int page) {
+        GeneralJournalBatch generalJournalBatch = svc.get(code);
+        return generalJournalBatchLineSvc.list(generalJournalBatch, page).stream()
+                .map(x -> generalJournalBatchLineSvc.toDTO(x))
+                .collect(Collectors.toList());
+    }
+
+    @GetMapping("/{code}/lines/{lineNo}")
+    public GeneralJournalBatchLineDTO getLine(@PathVariable("code") String code, @PathVariable("lineNo") int lineNo) {
+        GeneralJournalBatchLineId generalJournalBatchLineId = new GeneralJournalBatchLineId();
+        generalJournalBatchLineId.setGeneralJournalBatch(svc.get(code));
+        generalJournalBatchLineId.setLineNo(lineNo);
+
+        return generalJournalBatchLineSvc.toDTO(generalJournalBatchLineSvc.get(generalJournalBatchLineId));
+    }
+
+    @PostMapping("/{code}/lines")
+    public int postLine(@PathVariable("code") String code, @RequestBody GeneralJournalBatchLineDTO dto) {
+        GeneralJournalBatchLine generalJournalBatchLine = generalJournalBatchLineSvc.fromDTO(dto);
+        generalJournalBatchLineSvc.create(generalJournalBatchLine);
+        return 0;
+    }
+
+    @PutMapping("/{code}/lines/{lineNo}")
+    public int putLine(@PathVariable("code") String code, @PathVariable("lineNo") int lineNo,
+            @RequestBody GeneralJournalBatchLineDTO dto) {
+        GeneralJournalBatch generalJournalBatch = svc.get(code);
+
+        GeneralJournalBatchLineId generalJournalBatchLineId = new GeneralJournalBatchLineId();
+        generalJournalBatchLineId.setGeneralJournalBatch(generalJournalBatch);
+        generalJournalBatchLineId.setLineNo(lineNo);
+
+        GeneralJournalBatchLine generalJournalBatchLine = generalJournalBatchLineSvc.fromDTO(dto);
+
+        generalJournalBatchLineSvc.update(generalJournalBatchLineId, generalJournalBatchLine);
+
+        return lineNo;
+    }
+
+    @DeleteMapping("/{code}/lines/{lineNo}")
+    public int deleteLine(@PathVariable("code") String code, @PathVariable("lineNo") int lineNo) {
+        GeneralJournalBatch generalJournalBatch = svc.get(code);
+        
+        GeneralJournalBatchLineId generalJournalBatchLineId = new GeneralJournalBatchLineId();
+        generalJournalBatchLineId.setGeneralJournalBatch(generalJournalBatch);
+        generalJournalBatchLineId.setLineNo(lineNo);
+
+        generalJournalBatchLineSvc.delete(generalJournalBatchLineId);
+        
+        return lineNo;
     }
 
     @PostMapping
@@ -47,11 +108,11 @@ public class GeneralJournalBatchController {
     }
 
     @PutMapping("/{code}")
-    public String put(@PathVariable("code") String code,  @RequestBody GeneralJournalBatchDTO dto) {
+    public String put(@PathVariable("code") String code, @RequestBody GeneralJournalBatchDTO dto) {
         GeneralJournalBatch generalJournalBatch = svc.fromDTO(dto);
         svc.update(code, generalJournalBatch);
         return code;
-    } 
+    }
 
     @DeleteMapping("/{code}")
     public String delete(@PathVariable("code") String code) {
--- erp.backend/src/test/java/org/roko/erp/backend/controllers/GeneralJournalBatchControllerTest.java
+++ erp.backend/src/test/java/org/roko/erp/backend/controllers/GeneralJournalBatchControllerTest.java
@@ -10,14 +10,20 @@ import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.roko.erp.backend.model.GeneralJournalBatch;
+import org.roko.erp.backend.model.GeneralJournalBatchLine;
+import org.roko.erp.backend.model.jpa.GeneralJournalBatchLineId;
+import org.roko.erp.backend.services.GeneralJournalBatchLineService;
 import org.roko.erp.backend.services.GeneralJournalBatchService;
 import org.roko.erp.model.dto.GeneralJournalBatchDTO;
+import org.roko.erp.model.dto.GeneralJournalBatchLineDTO;
 
 public class GeneralJournalBatchControllerTest {
-    
+
     private static final String TEST_CODE = "test-code";
 
     private static final int TEST_PAGE = 123;
+
+    private static final int TEST_LINE_NO = 1234;
 
     @Mock
     private GeneralJournalBatch generalJournalBatchMock;
@@ -28,17 +34,36 @@ public class GeneralJournalBatchControllerTest {
     @Mock
     private GeneralJournalBatchService svcMock;
 
+    @Mock
+    private GeneralJournalBatchLineService generalJournalBatchLineSvcMock;
+
+    @Mock
+    private GeneralJournalBatchLine generalJournalBatchLineMock;
+
+    @Mock
+    private GeneralJournalBatchLineDTO generalJournalBatchLineDtoMock;
+
     private GeneralJournalBatchController controller;
 
     @BeforeEach
     public void setup() {
         MockitoAnnotations.openMocks(this);
 
+        GeneralJournalBatchLineId generalJournalBatchLineId = new GeneralJournalBatchLineId();
+        generalJournalBatchLineId.setGeneralJournalBatch(generalJournalBatchMock);
+        generalJournalBatchLineId.setLineNo(TEST_LINE_NO);
+
+        when(generalJournalBatchLineSvcMock.list(generalJournalBatchMock, TEST_PAGE))
+                .thenReturn(Arrays.asList(generalJournalBatchLineMock));
+        when(generalJournalBatchLineSvcMock.get(generalJournalBatchLineId)).thenReturn(generalJournalBatchLineMock);
+        when(generalJournalBatchLineSvcMock.fromDTO(generalJournalBatchLineDtoMock))
+                .thenReturn(generalJournalBatchLineMock);
+
         when(svcMock.list(TEST_PAGE)).thenReturn(Arrays.asList(generalJournalBatchMock));
         when(svcMock.get(TEST_CODE)).thenReturn(generalJournalBatchMock);
         when(svcMock.fromDTO(dtoMock)).thenReturn(generalJournalBatchMock);
 
-        controller = new GeneralJournalBatchController(svcMock);
+        controller = new GeneralJournalBatchController(svcMock, generalJournalBatchLineSvcMock);
     }
 
     @Test
@@ -76,5 +101,54 @@ public class GeneralJournalBatchControllerTest {
         controller.delete(TEST_CODE);
 
         verify(svcMock).delete(TEST_CODE);
+    }
+
+    @Test
+    public void listLines_delegatesToService() {
+        controller.listLines(TEST_CODE, TEST_PAGE);
+
+        verify(generalJournalBatchLineSvcMock).list(generalJournalBatchMock, TEST_PAGE);
+        verify(generalJournalBatchLineSvcMock).toDTO(generalJournalBatchLineMock);
+    }
+
+    @Test
+    public void getLine_delegatesToService() {
+        controller.getLine(TEST_CODE, TEST_LINE_NO);
+
+        GeneralJournalBatchLineId generalJournalBatchLineId = new GeneralJournalBatchLineId();
+        generalJournalBatchLineId.setGeneralJournalBatch(generalJournalBatchMock);
+        generalJournalBatchLineId.setLineNo(TEST_LINE_NO);
+
+        verify(generalJournalBatchLineSvcMock).get(generalJournalBatchLineId);
+        verify(generalJournalBatchLineSvcMock).toDTO(generalJournalBatchLineMock);
+    }
+
+    @Test
+    public void postLine_delegatesToService() {
+        controller.postLine(TEST_CODE, generalJournalBatchLineDtoMock);
+
+        verify(generalJournalBatchLineSvcMock).create(generalJournalBatchLineMock);
+    }
+
+    @Test
+    public void putLine_delegatesToService() {
+        controller.putLine(TEST_CODE, TEST_LINE_NO, generalJournalBatchLineDtoMock);
+
+        GeneralJournalBatchLineId generalJournalBatchLineId = new GeneralJournalBatchLineId();
+        generalJournalBatchLineId.setGeneralJournalBatch(generalJournalBatchMock);
+        generalJournalBatchLineId.setLineNo(TEST_LINE_NO);
+
+        verify(generalJournalBatchLineSvcMock).update(generalJournalBatchLineId, generalJournalBatchLineMock);
+    }
+
+    @Test
+    public void deleteLine_delegatesToService() {
+        controller.deleteLine(TEST_CODE, TEST_LINE_NO);
+
+        GeneralJournalBatchLineId generalJournalBatchLineId = new GeneralJournalBatchLineId();
+        generalJournalBatchLineId.setGeneralJournalBatch(generalJournalBatchMock);
+        generalJournalBatchLineId.setLineNo(TEST_LINE_NO);
+
+        verify(generalJournalBatchLineSvcMock).delete(generalJournalBatchLineId);
     }
 }
