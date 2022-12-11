74190a4a7a0c9fd51b322fbccaf00e0cec70f37b
Rashid Rashidov <rrashidov@gmail.com>
2022-12-11 21:36:10 +0200

    Introduce CodeSerieDTO

---
+++ erp.model.dto/src/main/java/org/roko/erp/model/dto/CodeSerieDTO.java
@@ -0,0 +1,35 @@
+package org.roko.erp.model.dto;
+
+public class CodeSerieDTO {
+    
+    private String code = "";
+    private String name = "";
+    private String firstCode = "";
+    private String lastCode = "";
+    
+    public String getCode() {
+        return code;
+    }
+    public void setCode(String code) {
+        this.code = code;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getFirstCode() {
+        return firstCode;
+    }
+    public void setFirstCode(String firstCode) {
+        this.firstCode = firstCode;
+    }
+    public String getLastCode() {
+        return lastCode;
+    }
+    public void setLastCode(String lastCode) {
+        this.lastCode = lastCode;
+    }
+
+}
