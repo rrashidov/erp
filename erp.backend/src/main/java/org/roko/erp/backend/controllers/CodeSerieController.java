f5caa108d9388d006796db7d6c5d842227cf2a48
Rashid Rashidov <rrashidov@gmail.com>
2022-12-11 22:25:55 +0200

    [backend] Introduce CodeSerieController

---
+++ erp.backend/src/main/java/org/roko/erp/backend/controllers/CodeSerieController.java
@@ -0,0 +1,68 @@
+package org.roko.erp.backend.controllers;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.roko.erp.backend.model.CodeSerie;
+import org.roko.erp.backend.services.CodeSerieService;
+import org.roko.erp.model.dto.CodeSerieDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/codeseries")
+public class CodeSerieController {
+    
+    private CodeSerieService svc;
+
+    @Autowired
+    public CodeSerieController(CodeSerieService svc) {
+        this.svc = svc;
+    }
+
+    @GetMapping
+    public List<CodeSerieDTO> list() {
+        return svc.list().stream()
+            .map(x -> svc.toDTO(x))
+            .collect(Collectors.toList());
+    }
+
+    @GetMapping("/page/{page}")
+    public List<CodeSerieDTO> list(@PathVariable("page") int page) {
+        return svc.list(page).stream()
+            .map(x -> svc.toDTO(x))
+            .collect(Collectors.toList());
+    }
+
+    @GetMapping("/{code}")
+    public CodeSerieDTO get(@PathVariable("code") String code) {
+        return svc.toDTO(svc.get(code));
+    }
+
+    @PostMapping
+    public String post(@RequestBody CodeSerieDTO dto) {
+        CodeSerie codeSerie = svc.fromDTO(dto);
+        svc.create(codeSerie);
+        return codeSerie.getCode();
+    }
+
+    @PutMapping("/{code}")
+    public String put(@PathVariable("code") String code, @RequestBody CodeSerieDTO dto) {
+        CodeSerie codeSerie = svc.fromDTO(dto);
+        svc.update(code, codeSerie);
+        return code;
+    }
+
+    @DeleteMapping("/{code}")
+    public String delete(@PathVariable("code") String code) {
+        svc.delete(code);
+        return code;
+    }
+}
---
+++ erp.backend/src/test/java/org/roko/erp/backend/controllers/CodeSerieControllerTest.java
@@ -0,0 +1,89 @@
+package org.roko.erp.backend.controllers;
+
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.Arrays;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.roko.erp.backend.model.CodeSerie;
+import org.roko.erp.backend.services.CodeSerieService;
+import org.roko.erp.model.dto.CodeSerieDTO;
+
+public class CodeSerieControllerTest {
+    
+    private static final String TEST_CODE = "test-code";
+
+    private static final int TEST_PAGE = 123;
+
+    @Mock
+    private CodeSerieService svcMock;
+
+    @Mock
+    private CodeSerie codeSerieMock;
+
+    @Mock
+    private CodeSerieDTO dtoMock;
+
+    private CodeSerieController controller;
+
+    @BeforeEach
+    public void setup() {
+        MockitoAnnotations.openMocks(this);
+
+        when(svcMock.list()).thenReturn(Arrays.asList(codeSerieMock));
+        when(svcMock.list(TEST_PAGE)).thenReturn(Arrays.asList(codeSerieMock));
+        when(svcMock.get(TEST_CODE)).thenReturn(codeSerieMock);
+        when(svcMock.fromDTO(dtoMock)).thenReturn(codeSerieMock);
+
+        controller = new CodeSerieController(svcMock);
+    }
+
+    @Test
+    public void list_delegatesToService() {
+        controller.list();
+
+        verify(svcMock).list();
+        verify(svcMock).toDTO(codeSerieMock);
+    }
+
+    @Test
+    public void listPage_delegatesToService() {
+        controller.list(TEST_PAGE);
+
+        verify(svcMock).list(TEST_PAGE);
+        verify(svcMock).toDTO(codeSerieMock);
+    }
+
+    @Test
+    public void get_delegatesToService() {
+        controller.get(TEST_CODE);
+
+        verify(svcMock).get(TEST_CODE);
+        verify(svcMock).toDTO(codeSerieMock);
+    }
+
+    @Test
+    public void post_delegatesToService() {
+        controller.post(dtoMock);
+
+        verify(svcMock).create(codeSerieMock);
+    }
+
+    @Test
+    public void put_delegatesToService() {
+        controller.put(TEST_CODE, dtoMock);
+
+        verify(svcMock).update(TEST_CODE, codeSerieMock);
+    }
+
+    @Test
+    public void delete_delegatesToService() {
+        controller.delete(TEST_CODE);
+
+        verify(svcMock).delete(TEST_CODE);
+    }
+}
