c3f432e4911abcd7a02a683ea878911a497b2d3b
Rashid Rashidov <rrashidov@gmail.com>
2022-12-17 18:42:48 +0200

    [frontend] Remove posting code
    - code is available on backend and should be executed there

--- erp.frontend/src/main/java/org/roko/erp/frontend/services/PurchaseOrderPostServiceImpl.java
+++ erp.frontend/src/main/java/org/roko/erp/frontend/services/PurchaseOrderPostServiceImpl.java
@@ -1,39 +1,12 @@
 package org.roko.erp.frontend.services;
 
-import java.util.Date;
-import java.util.List;
-import java.util.Optional;
-
 import javax.transaction.Transactional;
 
-import org.roko.erp.frontend.model.BankAccount;
-import org.roko.erp.frontend.model.BankAccountLedgerEntry;
-import org.roko.erp.frontend.model.BankAccountLedgerEntryType;
-import org.roko.erp.frontend.model.ItemLedgerEntry;
-import org.roko.erp.frontend.model.ItemLedgerEntryType;
-import org.roko.erp.frontend.model.PostedPurchaseOrder;
-import org.roko.erp.frontend.model.PostedPurchaseOrderLine;
-import org.roko.erp.frontend.model.PurchaseOrder;
-import org.roko.erp.frontend.model.PurchaseOrderLine;
-import org.roko.erp.frontend.model.VendorLedgerEntry;
-import org.roko.erp.frontend.model.VendorLedgerEntryType;
-import org.roko.erp.frontend.model.jpa.PostedPurchaseOrderLineId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PurchaseOrderPostServiceImpl implements PurchaseOrderPostService {
-
-    private static final String NOT_ENOUGH_BANK_ACCOUNT_BALANCE_MSG = "Bank account %s has balance %.2f. Can not pay %.2f";;
-    private PurchaseOrderService purchaseOrderSvc;
-    private PurchaseOrderLineService purchaseOrderLineSvc;
-    private PostedPurchaseOrderService postedPurchaseOrderSvc;
-    private PostedPurchaseOrderLineService postedPurchaseOrderLineSvc;
-    private ItemLedgerEntryService itemLedgerEntrySvc;
-    private VendorLedgerEntryService vendorLedgerEntrySvc;
-    private BankAccountLedgerEntryService bankAccountLedgerEntrySvc;
-    private PurchaseCodeSeriesService purchaseCodeSeriesSvc;
-    private BankAccountService bankAccountSvc;
 
     @Autowired
     public PurchaseOrderPostServiceImpl(PurchaseOrderService purchaseOrderSvc,
@@ -41,160 +14,11 @@ public class PurchaseOrderPostServiceImpl implements PurchaseOrderPostService {
             PostedPurchaseOrderLineService postedPurchaseOrderLineSvc, ItemLedgerEntryService itemLedgerEntrySvc,
             VendorLedgerEntryService vendorLedgerEntrySvc, BankAccountLedgerEntryService bankAccountLedgerEntrySvc,
             BankAccountService bankAccountSvc, PurchaseCodeSeriesService purchaseCodeSeriesSvc) {
-        this.purchaseOrderSvc = purchaseOrderSvc;
-        this.purchaseOrderLineSvc = purchaseOrderLineSvc;
-        this.postedPurchaseOrderSvc = postedPurchaseOrderSvc;
-        this.postedPurchaseOrderLineSvc = postedPurchaseOrderLineSvc;
-        this.itemLedgerEntrySvc = itemLedgerEntrySvc;
-        this.vendorLedgerEntrySvc = vendorLedgerEntrySvc;
-        this.bankAccountLedgerEntrySvc = bankAccountLedgerEntrySvc;
-        this.bankAccountSvc = bankAccountSvc;
-        this.purchaseCodeSeriesSvc = purchaseCodeSeriesSvc;
     }
 
     @Override
     @Transactional(rollbackOn = PostFailedException.class)
     public void post(String code) throws PostFailedException {
-        PurchaseOrder purchaseOrder = purchaseOrderSvc.get(code);
-        List<PurchaseOrderLine> purchaseOrderLines = purchaseOrderLineSvc.list(purchaseOrder);
-
-        PostedPurchaseOrder postedPurchaseOrder = createPostedPurchaseOrder(purchaseOrder);
-
-        createPostedPurchaseOrderLines(purchaseOrderLines, postedPurchaseOrder);
-
-        createItemLedgerEntries(purchaseOrderLines, postedPurchaseOrder);
-
-        createVendorLedgerEntries(purchaseOrderLines, postedPurchaseOrder);
-
-        createBankAccountLedgerEntry(purchaseOrderLines, postedPurchaseOrder);
-
-        deletePurchaseOrderLines(purchaseOrderLines);
-
-        deletePurchaseOrder(code);
-    }
-
-    private void deletePurchaseOrder(String code) {
-        purchaseOrderSvc.delete(code);
-    }
-
-    private void deletePurchaseOrderLines(List<PurchaseOrderLine> purchaseOrderLines) {
-        purchaseOrderLines.stream()
-                .forEach(x -> purchaseOrderLineSvc.delete(x.getPurchaseOrderLineId()));
-    }
-
-    private void createBankAccountLedgerEntry(List<PurchaseOrderLine> purchaseOrderLines,
-            PostedPurchaseOrder postedPurchaseOrder) throws PostFailedException {
-        if (postedPurchaseOrder.getPaymentMethod().getBankAccount() == null) {
-            return;
-        }
-
-        Optional<Double> amount = purchaseOrderLines.stream()
-                .map(PurchaseOrderLine::getAmount)
-                .reduce((x, y) -> x + y);
-
-        BankAccount bankAccount = bankAccountSvc.get(postedPurchaseOrder.getPaymentMethod().getBankAccount().getCode());
-
-        if (bankAccount.getBalance() < amount.get()) {
-            throw new PostFailedException(String.format(NOT_ENOUGH_BANK_ACCOUNT_BALANCE_MSG, bankAccount.getCode(),
-                    bankAccount.getBalance(), amount.get()));
-        }
-
-        BankAccountLedgerEntry bankAccountLedgerEntry = new BankAccountLedgerEntry();
-        bankAccountLedgerEntry.setBankAccount(postedPurchaseOrder.getPaymentMethod().getBankAccount());
-        bankAccountLedgerEntry.setType(BankAccountLedgerEntryType.VENDOR_PAYMENT);
-        bankAccountLedgerEntry.setAmount(-amount.get());
-        bankAccountLedgerEntry.setDate(new Date());
-        bankAccountLedgerEntry.setDocumentCode(postedPurchaseOrder.getCode());
-
-        bankAccountLedgerEntrySvc.create(bankAccountLedgerEntry);
-    }
-
-    private void createVendorLedgerEntries(List<PurchaseOrderLine> purchaseOrderLines,
-            PostedPurchaseOrder postedPurchaseOrder) {
-        Optional<Double> amount = purchaseOrderLines.stream()
-                .map(PurchaseOrderLine::getAmount)
-                .reduce((x, y) -> x + y);
-
-        createDocumentVendorLedgerEntry(postedPurchaseOrder, amount.get());
-
-        if (postedPurchaseOrder.getPaymentMethod().getBankAccount() != null) {
-            createPaymentVendorLedgerEntry(postedPurchaseOrder, amount.get());
-        }
-    }
-
-    private void createPaymentVendorLedgerEntry(PostedPurchaseOrder postedPurchaseOrder, Double amount) {
-        VendorLedgerEntry vendorLedgerEntry = new VendorLedgerEntry();
-        vendorLedgerEntry.setVendor(postedPurchaseOrder.getVendor());
-        vendorLedgerEntry.setType(VendorLedgerEntryType.PAYMENT);
-        vendorLedgerEntry.setAmount(-amount);
-        vendorLedgerEntry.setDate(new Date());
-        vendorLedgerEntry.setDocumentCode(postedPurchaseOrder.getCode());
-
-        vendorLedgerEntrySvc.create(vendorLedgerEntry);
-    }
-
-    private void createDocumentVendorLedgerEntry(PostedPurchaseOrder postedPurchaseOrder, Double amount) {
-        VendorLedgerEntry vendorLedgerEntry = new VendorLedgerEntry();
-        vendorLedgerEntry.setVendor(postedPurchaseOrder.getVendor());
-        vendorLedgerEntry.setType(VendorLedgerEntryType.PURCHASE_ORDER);
-        vendorLedgerEntry.setAmount(amount);
-        vendorLedgerEntry.setDate(new Date());
-        vendorLedgerEntry.setDocumentCode(postedPurchaseOrder.getCode());
-
-        vendorLedgerEntrySvc.create(vendorLedgerEntry);
-    }
-
-    private void createItemLedgerEntries(List<PurchaseOrderLine> purchaseOrderLines,
-            PostedPurchaseOrder postedPurchaseOrder) {
-        purchaseOrderLines.stream()
-                .forEach(x -> createItemLedgerEntry(x, postedPurchaseOrder));
-    }
-
-    private void createItemLedgerEntry(PurchaseOrderLine purchaseOrderLine, PostedPurchaseOrder postedPurchaseOrder) {
-        ItemLedgerEntry itemLedgerEntry = new ItemLedgerEntry();
-        itemLedgerEntry.setItem(purchaseOrderLine.getItem());
-        itemLedgerEntry.setType(ItemLedgerEntryType.PURCHASE_ORDER);
-        itemLedgerEntry.setQuantity(purchaseOrderLine.getQuantity());
-        itemLedgerEntry.setDate(new Date());
-        itemLedgerEntry.setDocumentCode(postedPurchaseOrder.getCode());
-
-        itemLedgerEntrySvc.create(itemLedgerEntry);
-    }
-
-    private void createPostedPurchaseOrderLines(List<PurchaseOrderLine> purchaseOrderLines,
-            PostedPurchaseOrder postedPurchaseOrder) {
-        purchaseOrderLines.stream()
-                .forEach(x -> createPostedPurchaseOrderLine(x, postedPurchaseOrder));
-    }
-
-    private void createPostedPurchaseOrderLine(PurchaseOrderLine purchaseOrderLine,
-            PostedPurchaseOrder postedPurchaseOrder) {
-        PostedPurchaseOrderLineId postedPurchaseOrderLineId = new PostedPurchaseOrderLineId();
-        postedPurchaseOrderLineId.setPostedPurchaseOrder(postedPurchaseOrder);
-        postedPurchaseOrderLineId.setLineNo(purchaseOrderLine.getPurchaseOrderLineId().getLineNo());
-
-        PostedPurchaseOrderLine postedPurchaseOrderLine = new PostedPurchaseOrderLine();
-        postedPurchaseOrderLine.setPostedPurchaseOrderLineId(postedPurchaseOrderLineId);
-        postedPurchaseOrderLine.setItem(purchaseOrderLine.getItem());
-        postedPurchaseOrderLine.setQuantity(purchaseOrderLine.getQuantity());
-        postedPurchaseOrderLine.setPrice(purchaseOrderLine.getPrice());
-        postedPurchaseOrderLine.setAmount(purchaseOrderLine.getAmount());
-
-        postedPurchaseOrderLineSvc.create(postedPurchaseOrderLine);
-    }
-
-    private PostedPurchaseOrder createPostedPurchaseOrder(PurchaseOrder purchaseOrder) {
-        PostedPurchaseOrder postedPurchaseOrder = new PostedPurchaseOrder();
-        postedPurchaseOrder.setCode(purchaseCodeSeriesSvc.postedOrderCode());
-        postedPurchaseOrder.setVendor(purchaseOrder.getVendor());
-        postedPurchaseOrder.setDate(new Date());
-        postedPurchaseOrder.setPaymentMethod(purchaseOrder.getPaymentMethod());
-        postedPurchaseOrder.setOrderCode(purchaseOrder.getCode());
-        postedPurchaseOrder.setOrderDate(purchaseOrder.getDate());
-
-        postedPurchaseOrderSvc.create(postedPurchaseOrder);
-
-        return postedPurchaseOrder;
     }
 
 }
--- erp.frontend/src/main/java/org/roko/erp/frontend/services/SalesOrderPostServiceImpl.java
+++ erp.frontend/src/main/java/org/roko/erp/frontend/services/SalesOrderPostServiceImpl.java
@@ -1,211 +1,23 @@
 package org.roko.erp.frontend.services;
 
-import java.util.Date;
-import java.util.List;
-import java.util.Optional;
-
 import javax.transaction.Transactional;
 
-import org.roko.erp.frontend.model.BankAccountLedgerEntry;
-import org.roko.erp.frontend.model.BankAccountLedgerEntryType;
-import org.roko.erp.frontend.model.CustomerLedgerEntry;
-import org.roko.erp.frontend.model.CustomerLedgerEntryType;
-import org.roko.erp.frontend.model.Item;
-import org.roko.erp.frontend.model.ItemLedgerEntry;
-import org.roko.erp.frontend.model.ItemLedgerEntryType;
-import org.roko.erp.frontend.model.PostedSalesOrder;
-import org.roko.erp.frontend.model.PostedSalesOrderLine;
-import org.roko.erp.frontend.model.SalesOrder;
-import org.roko.erp.frontend.model.SalesOrderLine;
-import org.roko.erp.frontend.model.jpa.PostedSalesOrderLineId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class SalesOrderPostServiceImpl implements SalesOrderPostService {
 
-    private static final String NOT_ENOUGH_INVENTORY_MSG = "Item %s has inventory %.2f. Can not sell %.2f";
-
-    private SalesOrderService salesOrderSvc;
-    private SalesOrderLineService salesOrderLineSvc;
-    private PostedSalesOrderService postedSalesOrderSvc;
-    private PostedSalesOrderLineService postedSalesOrderLineSvc;
-    private ItemLedgerEntryService itemLedgerEntrySvc;
-    private CustomerLedgerEntryService customerLedgerEntrySvc;
-    private BankAccountLedgerEntryService bankAccountLedgerEntrySvc;
-    private SalesCodeSeriesService salesCodeSeriesSvc;
-    private ItemService itemSvc;
-
     @Autowired
     public SalesOrderPostServiceImpl(SalesOrderService salesOrderSvc, SalesOrderLineService salesOrderLineSvc,
             PostedSalesOrderService postedSalesOrderSvc, PostedSalesOrderLineService postedSalesOrderLineSvc,
             ItemService itemSvc, ItemLedgerEntryService itemLedgerEntrySvc, CustomerLedgerEntryService customerLedgerEntrySvc,
             BankAccountLedgerEntryService bankAccountLedgerEntrySvc, SalesCodeSeriesService salesCodeSeriesSvc) {
-        this.salesOrderSvc = salesOrderSvc;
-        this.salesOrderLineSvc = salesOrderLineSvc;
-        this.postedSalesOrderSvc = postedSalesOrderSvc;
-        this.postedSalesOrderLineSvc = postedSalesOrderLineSvc;
-        this.itemSvc = itemSvc;
-        this.itemLedgerEntrySvc = itemLedgerEntrySvc;
-        this.customerLedgerEntrySvc = customerLedgerEntrySvc;
-        this.bankAccountLedgerEntrySvc = bankAccountLedgerEntrySvc;
-        this.salesCodeSeriesSvc = salesCodeSeriesSvc;
     }
 
     @Override
     @Transactional(rollbackOn = PostFailedException.class)
     public void post(String code) throws PostFailedException {
-        SalesOrder salesOrder = salesOrderSvc.get(code);
-
-        PostedSalesOrder postedSalesOrder = createPostedSalesOrder(salesOrder);
-
-        createPostedSalesOrderLines(salesOrder, postedSalesOrder);
-
-        createItemLedgerEntries(salesOrder, postedSalesOrder);
-
-        createCustomerLedgerEntries(salesOrder, postedSalesOrder);
-
-        createBankAccountLedgerEntries(salesOrder, postedSalesOrder);
-
-        deleteSalesOrderLines(salesOrder);
-
-        deleteSalesOrder(salesOrder);
-    }
-
-    private void deleteSalesOrder(SalesOrder salesOrder) {
-        salesOrderSvc.delete(salesOrder.getCode());
-    }
-
-    private void deleteSalesOrderLines(SalesOrder salesOrder) {
-        List<SalesOrderLine> salesOrderLines = salesOrderLineSvc.list(salesOrder);
-
-        for (SalesOrderLine salesOrderLine : salesOrderLines) {
-            salesOrderLineSvc.delete(salesOrderLine.getSalesOrderLineId());
-        }
-    }
-
-    private void createBankAccountLedgerEntries(SalesOrder salesOrder, PostedSalesOrder postedSalesOrder) {
-        if (salesOrder.getPaymentMethod().getBankAccount() == null) {
-            return;
-        }
-
-        List<SalesOrderLine> salesOrderLines = salesOrderLineSvc.list(salesOrder);
-
-        Optional<Double> amount = salesOrderLines.stream()
-                .map(SalesOrderLine::getAmount)
-                .reduce((x, y) -> x + y);
-
-        createBankAccountLedgerEntry(postedSalesOrder, amount.get());
-    }
-
-    private void createBankAccountLedgerEntry(PostedSalesOrder postedSalesOrder, Double amount) {
-        BankAccountLedgerEntry bankAccountLedgerEntry = new BankAccountLedgerEntry();
-        bankAccountLedgerEntry.setBankAccount(postedSalesOrder.getPaymentMethod().getBankAccount());
-        bankAccountLedgerEntry.setType(BankAccountLedgerEntryType.CUSTOMER_PAYMENT);
-        bankAccountLedgerEntry.setAmount(amount);
-        bankAccountLedgerEntry.setDate(new Date());
-        bankAccountLedgerEntry.setDocumentCode(postedSalesOrder.getCode());
-
-        bankAccountLedgerEntrySvc.create(bankAccountLedgerEntry);
-    }
-
-    private void createCustomerLedgerEntries(SalesOrder salesOrder, PostedSalesOrder postedSalesOrder) {
-        List<SalesOrderLine> salesOrderLines = salesOrderLineSvc.list(salesOrder);
-
-        Optional<Double> amount = salesOrderLines.stream()
-                .map(SalesOrderLine::getAmount)
-                .reduce((x, y) -> x + y);
-
-        createSalesOrderLedgerEntry(postedSalesOrder, amount.get());
-        if (salesOrder.getPaymentMethod().getBankAccount() != null) {
-            createPaymentLedgerEntry(postedSalesOrder, amount.get());
-        }
-    }
-
-    private void createSalesOrderLedgerEntry(PostedSalesOrder postedSalesOrder, Double amount) {
-        CustomerLedgerEntry customerLedgerEntry = new CustomerLedgerEntry();
-        customerLedgerEntry.setCustomer(postedSalesOrder.getCustomer());
-        customerLedgerEntry.setType(CustomerLedgerEntryType.SALES_ORDER);
-        customerLedgerEntry.setAmount(amount);
-        customerLedgerEntry.setDate(new Date());
-        customerLedgerEntry.setDocumentCode(postedSalesOrder.getCode());
-
-        customerLedgerEntrySvc.create(customerLedgerEntry);
-    }
-
-    private void createPaymentLedgerEntry(PostedSalesOrder postedSalesOrder, Double amount) {
-        CustomerLedgerEntry customerLedgerEntry = new CustomerLedgerEntry();
-        customerLedgerEntry.setCustomer(postedSalesOrder.getCustomer());
-        customerLedgerEntry.setType(CustomerLedgerEntryType.PAYMENT);
-        customerLedgerEntry.setAmount(-amount);
-        customerLedgerEntry.setDate(new Date());
-        customerLedgerEntry.setDocumentCode(postedSalesOrder.getCode());
-
-        customerLedgerEntrySvc.create(customerLedgerEntry);
-    }
-
-    private void createItemLedgerEntries(SalesOrder salesOrder, PostedSalesOrder postedSalesOrder) throws PostFailedException {
-        List<SalesOrderLine> salesOrderLines = salesOrderLineSvc.list(salesOrder);
-
-        for (SalesOrderLine salesOrderLine : salesOrderLines) {
-            createItemLedgerEntry(salesOrderLine, postedSalesOrder);
-        }
-    }
-
-    private void createItemLedgerEntry(SalesOrderLine salesOrderLine, PostedSalesOrder postedSalesOrder) throws PostFailedException {
-        Item item = itemSvc.get(salesOrderLine.getItem().getCode());
-
-        double inventory = item.getInventory();
-
-        if (salesOrderLine.getQuantity() > inventory){
-            throw new PostFailedException(String.format(NOT_ENOUGH_INVENTORY_MSG, salesOrderLine.getItem().getCode(), inventory, salesOrderLine.getQuantity()));
-        }
-        
-        ItemLedgerEntry itemLedgerEntry = new ItemLedgerEntry();
-        itemLedgerEntry.setItem(salesOrderLine.getItem());
-        itemLedgerEntry.setType(ItemLedgerEntryType.SALES_ORDER);
-        itemLedgerEntry.setQuantity(-salesOrderLine.getQuantity());
-        itemLedgerEntry.setDate(new Date());
-        itemLedgerEntry.setDocumentCode(postedSalesOrder.getCode());
-
-        itemLedgerEntrySvc.create(itemLedgerEntry);
-    }
-
-    private PostedSalesOrder createPostedSalesOrder(SalesOrder salesOrder) {
-        PostedSalesOrder postedSalesOrder = new PostedSalesOrder();
-        postedSalesOrder.setCode(salesCodeSeriesSvc.postedOrderCode());
-        postedSalesOrder.setCustomer(salesOrder.getCustomer());
-        postedSalesOrder.setDate(new Date());
-        postedSalesOrder.setOrderCode(salesOrder.getCode());
-        postedSalesOrder.setOrderDate(salesOrder.getDate());
-        postedSalesOrder.setPaymentMethod(salesOrder.getPaymentMethod());
-
-        postedSalesOrderSvc.create(postedSalesOrder);
-
-        return postedSalesOrder;
-    }
-
-    private void createPostedSalesOrderLines(SalesOrder salesOrder, PostedSalesOrder postedSalesOrder) {
-        List<SalesOrderLine> salesOrderLines = salesOrderLineSvc.list(salesOrder);
-
-        for (SalesOrderLine salesOrderLine : salesOrderLines) {
-            createSalesOrderLine(salesOrderLine, postedSalesOrder);
-        }
-    }
-
-    private void createSalesOrderLine(SalesOrderLine salesOrderLine, PostedSalesOrder postedSalesOrder) {
-        PostedSalesOrderLineId postedSalesOrderLineId = new PostedSalesOrderLineId();
-        postedSalesOrderLineId.setPostedSalesOrder(postedSalesOrder);
-        postedSalesOrderLineId.setLineNo(salesOrderLine.getSalesOrderLineId().getLineNo());
-
-        PostedSalesOrderLine postedSalesOrderLine = new PostedSalesOrderLine();
-        postedSalesOrderLine.setPostedSalesOrderLineId(postedSalesOrderLineId);
-        postedSalesOrderLine.setItem(salesOrderLine.getItem());
-        postedSalesOrderLine.setQuantity(salesOrderLine.getQuantity());
-        postedSalesOrderLine.setPrice(salesOrderLine.getPrice());
-        postedSalesOrderLine.setAmount(salesOrderLine.getAmount());
-
-        postedSalesOrderLineSvc.create(postedSalesOrderLine);
     }
 
 }
